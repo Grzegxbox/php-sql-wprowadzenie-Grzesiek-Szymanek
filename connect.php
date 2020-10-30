151dbe998d20700a8a69101ffe7ba8504a3ad9ff
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-10-30 14:35:30 +0100

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "remotemysql.com";
+$username = "Rp4CxP6YkY";
+$password = "V0BMRFi2V3";
+$dbname = " Rp4CxP6YkY";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+?>
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+echo("<li>zad 1 - SELECT * FROM pracownicy where imie like '%a' </li>");
+
+$conn = new mysqli("remotemysql.com","Rp4CxP6YkY","V0BMRFi2V3","Rp4CxP6YkY");
+$result = $conn->query('SELECT * FROM pracownicy where imie like "%a"');
+            echo("<table border = 1>");
+            echo("<th>id</th>");
+            echo("<th>imie</th>");
+            echo("<th>dzial</th>");
+            echo("<th>zarobki</th>");
+                while($row = $result->fetch_assoc()) {
+        echo("<tr>");
+        echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td>");
+        echo("</tr>");
+    }
+    echo("</table>");
+
+?>
