6cd47113d68f540e5f7158d08d0aee775921706a
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-11-06 12:47:03 +0100

    Update pracownicy.php

--- pracownicy.php
+++ pracownicy.php
@@ -11,7 +11,7 @@
   <a class="navigation_link2" href="funkcjeAgregujace.php">Funkcję Agregujące</a>
 </div>
   
-<h1>STRONA Z PRACOWNIKAMI</h1>
+<h1>PRACOWNICY</h1>
  
 <?php
   $conn= new mysqli("remotemysql.com","Rp4CxP6YkY","V0BMRFi2V3","Rp4CxP6YkY");
