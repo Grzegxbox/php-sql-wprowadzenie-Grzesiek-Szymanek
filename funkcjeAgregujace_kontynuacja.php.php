5c0aca3caf7ecc5dd9c28356e8ab98415af2f10b
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-11-06 13:56:36 +0100

    Create funkcjeAgregujace_kontynuacja.php.php

---
+++ funkcjeAgregujace_kontynuacja.php.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+  
+<link rel="stylesheet" href="style.css">
+<a class="mojlink0" href="https://github.com/SK-2019/php-sql-wprowadzenie-Grzesiek-Szymanek">Kliknij aby zobaczyć wszystko</a>
+  
+<div class="nav">
+  <a class="navigation_link3" href="pracownicyiorganizacja.php">Pracownicy I Organizacja</a>
+</div>
+  
+
+<h1>KONTYNUACJA FUNKCJI AGREGUJĄCYCH</h1>
+
+
+<?php
+
+
+
+?>
+
+</head>
+</html>
