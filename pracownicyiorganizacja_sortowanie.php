d5930d4aa5e89e90f67a166226255bb7b6a2f03a
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-11-06 13:37:21 +0100

    Update pracownicyiorganizacja_sortowanie.php

--- pracownicyiorganizacja_sortowanie.php
+++ pracownicyiorganizacja_sortowanie.php
@@ -12,9 +12,24 @@
 <h1>WYKORZYSTANIE FUNKCJI 'ORDER BY'</h1>
  
 <?php
-  echo("<h3>Pracownicy Posortowani Malejąco</h3>");
+  echo("<h3>Pracownicy Posortowani Malejąco Według Imienia</h3>");
     $conn = new mysqli("remotemysql.com","Rp4CxP6YkY","V0BMRFi2V3","Rp4CxP6YkY");
-    $result = $conn -> query('SELECT * FROM pracownicy ORDER BY dzial asc');
+    $result = $conn -> query('SELECT * FROM pracownicy ORDER BY imie desc');
+        echo("<table border=1>");
+        echo("<th>ID</th>");
+        echo("<th>Imie</th>");
+        echo("<th>Dzial</th>");
+        echo("<th>Zarobki</th>");
+            while($row = $result -> fetch_assoc()){
+                echo("<tr>");
+                    echo("<td>" .$row["id_pracownicy"]. "</td><td>" .$row["imie"]. "</td><td>" .$row["dzial"]. "</td><td>" .$row["zarobki"]. "</td>");
+                echo("</tr>");
+            }
+        echo("</table>");
+  
+  echo("<h3>Pracownicy Z Działu 3 Posortowani Rosnąco Po Imieniu</h3>");
+    $conn = new mysqli("remotemysql.com","Rp4CxP6YkY","V0BMRFi2V3","Rp4CxP6YkY");
+    $result = $conn -> query('SELECT * FROM pracownicy WHERE dzial=3 ORDER BY imie asc');
         echo("<table border=1>");
         echo("<th>ID</th>");
         echo("<th>Imie</th>");
