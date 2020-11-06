c78b24eff3fe3f219d3ec0a1079d2f04445d036f
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-11-06 12:55:04 +0100

    Create pracownicyiorganizacja.php

---
+++ pracownicyiorganizacja.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  
+<link rel="stylesheet" href="style.css">
+<a class="mojlink0" href="https://github.com/SK-2019/php-sql-wprowadzenie-Grzesiek-Szymanek">Kliknij aby zobaczyć wszystko</a>
+  
+<div class="nav">
+  <a class="navigation_link0" href="https://nieumiem.herokuapp.com/">Strona Główna</a>
+  <a class="navigation_link1" href="pracownicy.php">Pracownicy</a>
+  <a class="navigation_link2" href="funkcjeAgregujace.php">Funkcje Agregujące</a>
+  <a class="navigation_link3" href="pracownicyiorganizacja.php">Pracownicy I Organizacja</a>
+</div>
+  
+<h1>PRACOWNICY I ORGANIZACJA</h1>
