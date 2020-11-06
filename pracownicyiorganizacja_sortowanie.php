d70da6dbe77fc3b1156c63a73eb0bbe25ddfade2
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-11-06 13:47:31 +0100

    Update pracownicyiorganizacja_sortowanie.php

--- pracownicyiorganizacja_sortowanie.php
+++ pracownicyiorganizacja_sortowanie.php
@@ -12,47 +12,82 @@
 <h1>WYKORZYSTANIE FUNKCJI 'ORDER BY'</h1>
  
 <?php
-  echo("<h3>Pracownicy Posortowani Malejąco Według Imienia</h3>");
+  echo("<h3>Pracownicy Posortowani Malejąco Według Imienia I Nazwy Działów</h3>");
     $conn = new mysqli("remotemysql.com","Rp4CxP6YkY","V0BMRFi2V3","Rp4CxP6YkY");
-    $result = $conn -> query('SELECT * FROM pracownicy ORDER BY imie desc');
+    $result = $conn -> query('SELECT * FROM pracownicy, organizacja WHERE dzial=id_org ORDER BY imie desc');
         echo("<table border=1>");
         echo("<th>ID</th>");
         echo("<th>Imie</th>");
         echo("<th>Dzial</th>");
         echo("<th>Zarobki</th>");
+        echo("<th>Nazwa Działu</th>"); 
             while($row = $result -> fetch_assoc()){
                 echo("<tr>");
-                    echo("<td>" .$row["id_pracownicy"]. "</td><td>" .$row["imie"]. "</td><td>" .$row["dzial"]. "</td><td>" .$row["zarobki"]. "</td>");
+                    echo("<td>" .$row["id_pracownicy"]. "</td><td>" .$row["imie"]. "</td><td>" .$row["dzial"]. "</td><td>" .$row["zarobki"]. "</td><td>" .$row["nazwa_dzial"]. "</td>");
                 echo("</tr>");
             }
         echo("</table>");
   
-  echo("<h3>Pracownicy Z Działu 3 Posortowani Rosnąco Po Imieniu</h3>");
+  echo("<h3>Pracownicy Z Działu 3 Posortowani Rosnąco Po Imieniu I Nazwy Działów</h3>");
     $conn = new mysqli("remotemysql.com","Rp4CxP6YkY","V0BMRFi2V3","Rp4CxP6YkY");
-    $result = $conn -> query('SELECT * FROM pracownicy WHERE dzial=3 ORDER BY imie asc');
+    $result = $conn -> query('SELECT * FROM pracownicy, organizacja WHERE (dzial=id_org) AND (dzial=3) ORDER BY imie asc');
         echo("<table border=1>");
         echo("<th>ID</th>");
         echo("<th>Imie</th>");
         echo("<th>Dzial</th>");
         echo("<th>Zarobki</th>");
+        echo("<th>Nazwa Działu</th>"); 
             while($row = $result -> fetch_assoc()){
                 echo("<tr>");
-                    echo("<td>" .$row["id_pracownicy"]. "</td><td>" .$row["imie"]. "</td><td>" .$row["dzial"]. "</td><td>" .$row["zarobki"]. "</td>");
+                    echo("<td>" .$row["id_pracownicy"]. "</td><td>" .$row["imie"]. "</td><td>" .$row["dzial"]. "</td><td>" .$row["zarobki"]. "</td><td>" .$row["nazwa_dzial"]. "</td>");
                 echo("</tr>");
             }
         echo("</table>");
   
-  echo("<h3>Pracownicy Z Działu 3 Posortowani Rosnąco Po Imieniu</h3>");
+  echo("<h3>Kobiety Posortowane Rosnąco Po Imieniu I Nazwy Działów</h3>");
     $conn = new mysqli("remotemysql.com","Rp4CxP6YkY","V0BMRFi2V3","Rp4CxP6YkY");
-    $result = $conn -> query('SELECT * FROM pracownicy WHERE dzial=3 ORDER BY imie asc');
+    $result = $conn -> query('SELECT * FROM pracownicy, organizacja WHERE (dzial=id_org) AND (imie like "%a") ORDER BY imie asc');
         echo("<table border=1>");
         echo("<th>ID</th>");
         echo("<th>Imie</th>");
         echo("<th>Dzial</th>");
         echo("<th>Zarobki</th>");
+        echo("<th>Nazwa Działu</th>");
             while($row = $result -> fetch_assoc()){
                 echo("<tr>");
-                    echo("<td>" .$row["id_pracownicy"]. "</td><td>" .$row["imie"]. "</td><td>" .$row["dzial"]. "</td><td>" .$row["zarobki"]. "</td>");
+                    echo("<td>" .$row["id_pracownicy"]. "</td><td>" .$row["imie"]. "</td><td>" .$row["dzial"]. "</td><td>" .$row["zarobki"]. "</td><td>" .$row["nazwa_dzial"]. "</td>");
+                echo("</tr>");
+            }
+        echo("</table>");
+  
+  echo("<h3>Kobiety Z Działu 1 I 3 Posortowane Rosnąco Po Zarobkach I Nazwy Działów</h3>");
+    $conn = new mysqli("remotemysql.com","Rp4CxP6YkY","V0BMRFi2V3","Rp4CxP6YkY");
+    $result = $conn -> query('SELECT * FROM pracownicy, organizacja WHERE (dzial=id_org) AND (imie like "%a") ORDER BY zarobki asc');
+        echo("<table border=1>");
+        echo("<th>ID</th>");
+        echo("<th>Imie</th>");
+        echo("<th>Dzial</th>");
+        echo("<th>Zarobki</th>");
+        echo("<th>Nazwa Działu</th>");
+            while($row = $result -> fetch_assoc()){
+                echo("<tr>");
+                    echo("<td>" .$row["id_pracownicy"]. "</td><td>" .$row["imie"]. "</td><td>" .$row["dzial"]. "</td><td>" .$row["zarobki"]. "</td><td>" .$row["nazwa_dzial"]. "</td>");
+                echo("</tr>");
+            }
+        echo("</table>");
+  
+  echo("<h3>Mężczyźni Posortowani Rosnąco Po Nazwie Działu A Następnie Po Wysokości Zarobków</h3>");
+    $conn = new mysqli("remotemysql.com","Rp4CxP6YkY","V0BMRFi2V3","Rp4CxP6YkY");
+    $result = $conn -> query('SELECT * FROM pracownicy, organizacja WHERE (dzial=id_org) AND (imie not like "%a") ORDER BY nazwa_dzial asc, zarobki asc');
+        echo("<table border=1>");
+        echo("<th>ID</th>");
+        echo("<th>Imie</th>");
+        echo("<th>Dzial</th>");
+        echo("<th>Zarobki</th>");
+        echo("<th>Nazwa Działu</th>");
+            while($row = $result -> fetch_assoc()){
+                echo("<tr>");
+                    echo("<td>" .$row["id_pracownicy"]. "</td><td>" .$row["imie"]. "</td><td>" .$row["dzial"]. "</td><td>" .$row["zarobki"]. "</td><td>" .$row["nazwa_dzial"]. "</td>");
                 echo("</tr>");
             }
         echo("</table>");
