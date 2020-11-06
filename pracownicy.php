21b41e7970d1dd33b89e49bac5cbb05529472d6b
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-11-06 09:35:29 +0100

    Update pracownicy.php

--- pracownicy.php
+++ pracownicy.php
@@ -1,3 +1,21 @@
-<?php
-echo("Tutaj, tutaj:PRACOWNICY");
-?>
+<!DOCTYPE html>
+<html>
+<head>
+  
+<link rel="stylesheet" href="style.css">
+<a class="mojlink0" href="https://github.com/SK-2019/php-sql-wprowadzenie-Grzesiek-Szymanek">Kliknij aby zobaczyć wszystko</a>
+  
+<div class="nav">
+  <a class="navigation_link0" href="https://nieumiem.herokuapp.com/">Strona Główna</a>
+  <a class="navigation_link1" href="pracownicy.php">Pracownicy</a>
+  <a class="navigation_link2" href="funkcjeAgregujace.php">Funkcję Agregujące</a>
+</div>
+  
+<h1>GRZEGORZ SZYMANEK Nr28</h1>
+ 
+  
+
+  
+  
+  </head>
+</html>
