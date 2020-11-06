b93edcf821f528378f0df73870bce84f89785a64
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-11-06 08:40:03 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <a href="https://github.com/SK-2019/php-sql-wprowadzenie-Grzesiek-Szymanek">Kliknij aby zobaczyć wszystko</a>
   
 <div class="nav">
-  <a class="navigation_link1" href="pracownicy.php">Pracownicy - wstęp</a>
+  <a class="navigation_link1" href="pracownicy.php">Pracownicy</a>
   <a class="navigation_link2" href="funkcjeAgregujace.php">Funkcję Agregujące</a>
   </div>
   
