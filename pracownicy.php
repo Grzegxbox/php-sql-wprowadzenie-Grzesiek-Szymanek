9525b7dd37000537d5efda3f89a48d64d87ecdcc
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-11-06 12:48:38 +0100

    Update pracownicy.php

--- pracownicy.php
+++ pracownicy.php
@@ -8,7 +8,7 @@
 <div class="nav">
   <a class="navigation_link0" href="https://nieumiem.herokuapp.com/">Strona Główna</a>
   <a class="navigation_link1" href="pracownicy.php">Pracownicy</a>
-  <a class="navigation_link2" href="funkcjeAgregujace.php">Funkcję Agregujące</a>
+  <a class="navigation_link2" href="funkcjeAgregujace.php">Funkcje Agregujące</a>
 </div>
   
 <h1>PRACOWNICY</h1>
