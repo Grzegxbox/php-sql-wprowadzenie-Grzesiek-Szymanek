7a0513d91d43483f02d54ae35de0cfbae3def2f0
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-11-06 13:31:18 +0100

    Update pracownicyiorganizacja_sortowanie.php

--- pracownicyiorganizacja_sortowanie.php
+++ pracownicyiorganizacja_sortowanie.php
@@ -12,6 +12,20 @@
 <h1>WYKORZYSTANIE FUNKCJI 'ORDER BY'</h1>
  
 <?php
+  echo("<h3>Pracownicy Posortowani Malejąco</h3>");
+    $conn = new mysqli("remotemysql.com","Rp4CxP6YkY","V0BMRFi2V3","Rp4CxP6YkY");
+    $result = $conn -> query('SELECT * FROM pracownicy ORDER BY imie desc');
+        echo("<table border=1>");
+        echo("<th>ID</th>");
+        echo("<th>Imie</th>");
+        echo("<th>Dzial</th>");
+        echo("<th>Zarobki</th>");
+            while($row = $result -> fetch_assoc()){
+                echo("<tr>");
+                    echo("<td>" .$row["id_pracownicy"]. "</td><td>" .$row["imie"]. "</td><td>" .$row["dzial"]. "</td><td>" .$row["zarobki"]. "</td>");
+                echo("</tr>");
+            }
+        echo("</table>");
   
   
   
