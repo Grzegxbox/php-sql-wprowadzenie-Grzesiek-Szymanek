eadf60a838431c2736f9248fd24076d39c701410
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-11-06 12:56:31 +0100

    Update pracownicy.php

--- pracownicy.php
+++ pracownicy.php
@@ -9,6 +9,7 @@
   <a class="navigation_link0" href="https://nieumiem.herokuapp.com/">Strona Główna</a>
   <a class="navigation_link1" href="pracownicy.php">Pracownicy</a>
   <a class="navigation_link2" href="funkcjeAgregujace.php">Funkcje Agregujące</a>
+  <a class="navigation_link3" href="pracownicyiorganizacja.php">Pracownicy I Organizacja</a>
 </div>
   
 <h1>PRACOWNICY</h1>
