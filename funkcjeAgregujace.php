c7ebbaf2c2528b9c851514eee55433e3e3ae76f7
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-11-06 09:15:25 +0100

    Update funkcjeAgregujace.php

--- funkcjeAgregujace.php
+++ funkcjeAgregujace.php
@@ -1,3 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+  
+<link rel="stylesheet" href="style.css">
+<a class="mojlink0" href="https://github.com/SK-2019/php-sql-wprowadzenie-Grzesiek-Szymanek">Kliknij aby zobaczyć wszystko</a>
+  
+<div class="nav">
+  <a class="navigation_link1" href="pracownicy.php">Pracownicy</a>
+  <a class="navigation_link2" href="funkcjeAgregujace.php">Funkcję Agregujące</a>
+</div>
+  
+<h1>GRZEGORZ SZYMANEK Nr28</h1>
+  <h2>Funkcję Agregujące</h2>
+  
+  
 <?php
-echo("Tutaj, tutaj: Funkcję Agregujące");
-?>
+ 
+
+echo("<h3>ZADANIE 1 - SELECT dzial, max(zarobki) AS Maksimum FROM pracownicy GROUP BY dzial</h3>");
+    $conn = new mysqli("remotemysql.com","Rp4CxP6YkY","V0BMRFi2V3","Rp4CxP6YkY");
+    $result = $conn -> query('SELECT dzial, max(zarobki) AS Maksimum FROM pracownicy GROUP BY dzial');
+        echo("<table border=1>");
+        echo("<th>DZIAŁ</th>");
+        echo("<th>MAKSIMUM</th>");
+            while($row = $result -> fetch_assoc()){
+                echo("<tr>");
+                    echo("<td>" .$row["dzial"]. "</td><td>" .$row["Maksimum"]. "</td>");
+                echo("</tr>");
+            }
+        echo("</table>");
+
+echo("<h3>ZADANIE 2 - SELECT Dzial, sum(zarobki) AS Całość, avg(zarobki) AS Średnia FROM pracownicy GROUP BY dzial</h3>");
+    $conn = new mysqli("remotemysql.com","Rp4CxP6YkY","V0BMRFi2V3","Rp4CxP6YkY");
+    $result = $conn -> query('SELECT Dzial, sum(zarobki) AS Całość, avg(zarobki) AS Średnia FROM pracownicy GROUP BY dzial');
+        echo("<table border=1>");
+        echo("<th>DZIAŁ</th>");
+        echo("<th>CAŁOŚĆ</th>");
+        echo("<th>ŚREDNIA</th>");
+            while($row = $result -> fetch_assoc()){
+                echo("<tr>");
+                    echo("<td>" .$row["Dzial"]. "</td><td>" .$row["Całość"]. "</td><td>" .$row["Średnia"]. "</td>");
+                echo("</tr>");
+            }
+        echo("</table>");
+
+echo("<h3>ZADANIE 3 - SELECT Dzial, Max(zarobki) AS Maksimum, Min(zarobki) AS Minimum, Sum(zarobki) AS Calosc, Avg(zarobki) AS Srednia FROM pracownicy GROUP BY dzial</h3>");
+    $conn = new mysqli("remotemysql.com","Rp4CxP6YkY","V0BMRFi2V3","Rp4CxP6YkY");
+    $result = $conn -> query('SELECT Dzial, Max(zarobki) AS Maksimum, Min(zarobki) AS Minimum, Sum(zarobki) AS Calosc, Avg(zarobki) AS Srednia FROM pracownicy GROUP BY dzial');
+        echo("<table border=1>");
+        echo("<th>DZIAŁ</th>");
+        echo("<th>MAKSIMUM</th>");
+        echo("<th>MINIMUM</th>");
+        echo("<th>CAŁOŚĆ</th>");
+        echo("<th>ŚREDNIA</th>");
+            while($row = $result -> fetch_assoc()){
+                echo("<tr>");
+                    echo("<td>" .$row["Dzial"]. "</td><td>" .$row["Maksimum"]. "</td><td>" .$row["Minimum"]. "</td><td>" .$row["Calosc"]. "</td><td>" .$row["Srednia"]. "</td>");
+                echo("</tr>");
+            }
+        echo("</table>");
