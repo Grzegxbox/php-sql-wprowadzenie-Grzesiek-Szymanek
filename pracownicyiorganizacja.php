4e2a32272d8d9f1c576ebcbf650c45e489a5502f
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-11-06 13:08:37 +0100

    Update pracownicyiorganizacja.php

--- pracownicyiorganizacja.php
+++ pracownicyiorganizacja.php
@@ -64,4 +64,22 @@
                 echo("</tr>");
             }
         echo("</table>");
+  
+    echo("<h3>Sami Mężczyźni Wraz Z Nazwami Działów</h3>");
+  $conn= new mysqli("remotemysql.com","Rp4CxP6YkY","V0BMRFi2V3","Rp4CxP6YkY");
+    $result=$conn->query('SELECT * FROM pracownicy, organizacja WHERE (imie not like "%a") AND (dzial=id_org)');
+        echo("<table border=2>");
+        echo("<th>ID</th>");
+        echo("<th>IMIĘ</th>");
+        echo("<th>DZIAŁ</th>");
+        echo("<th>ZAROBKI</th>");
+        echo("<th>NAZWA DZIAŁU</th>");
+            while($row=$result->fetch_assoc()){
+                echo("<tr>");
+                    echo("<td>" .$row["id_pracownicy"]. "</td><td>" .$row["imie"]. "</td><td>" .$row["dzial"]. "</td><td>" .$row["zarobki"]. "</td><td>" .$row["nazwa_dzial"]. "</td>");
+                echo("</tr>");
+            }
+        echo("</table>");
+  
+  
 ?>
