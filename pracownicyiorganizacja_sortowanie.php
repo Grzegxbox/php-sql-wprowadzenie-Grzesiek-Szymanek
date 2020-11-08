7841f6bfb6cc2847fa803d33eba4470436bf93d7
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-11-08 20:08:05 +0100

    Update pracownicyiorganizacja_sortowanie.php

--- pracownicyiorganizacja_sortowanie.php
+++ pracownicyiorganizacja_sortowanie.php
@@ -6,7 +6,7 @@
 <a class="mojlink0" href="https://github.com/SK-2019/php-sql-wprowadzenie-Grzesiek-Szymanek">Kliknij aby zobaczyć wszystko</a>
   
 <div class="nav">
-  <a class="navigation_link3" href="pracownicyiorganizacja.php">Pracownicy I Organizacja</a>
+  <a class="navigation_link1" href="pracownicyiorganizacja.php">Pracownicy I Organizacja</a>
 </div>
   
 <h1>WYKORZYSTANIE FUNKCJI 'ORDER BY'</h1>
@@ -91,13 +91,6 @@
                 echo("</tr>");
             }
         echo("</table>");
-  
-  
-  
-  
-  
-  
-  
 ?>
   
   
