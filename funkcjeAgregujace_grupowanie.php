941b93416051da6b1bcf8744c80a754ead723614
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-11-08 20:06:41 +0100

    Update funkcjeAgregujace_grupowanie.php

--- funkcjeAgregujace_grupowanie.php
+++ funkcjeAgregujace_grupowanie.php
@@ -6,7 +6,7 @@
 <a class="mojlink0" href="https://github.com/SK-2019/php-sql-wprowadzenie-Grzesiek-Szymanek">Kliknij aby zobaczyć wszystko</a>
   
 <div class="nav">
-  <a class="navigation_link2" href="funkcjeAgregujace.php">Funkcje Agregujące</a>
+  <a class="navigation_link1" href="funkcjeAgregujace.php">Funkcje Agregujące</a>
 </div>
   
 
