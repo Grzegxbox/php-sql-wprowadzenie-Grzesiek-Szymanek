d2398092eb122ab83548bfd964284e18c1653e6f
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-11-08 20:33:03 +0100

    Update funkcjeAgregujace_klauzula_HAVING.php

--- funkcjeAgregujace_klauzula_HAVING.php
+++ funkcjeAgregujace_klauzula_HAVING.php
@@ -15,42 +15,42 @@
 <?php
   echo("<h3>1. Średnia Zarobków W Poszczególnych Działach Mniejsza Od 28</h3>");
       $conn = new mysqli("remotemysql.com","Rp4CxP6YkY","V0BMRFi2V3","Rp4CxP6YkY");
-      $result = $conn -> query('SELECT dzial, nazwa_dzial, AVG(zarobki) AS srednia FROM pracownicy, organizacja WHERE (dzial=id_org) GROUP BY dzial HAVING AVG(zarobki)<28');
+      $result = $conn -> query('SELECT dzial, AVG(zarobki) AS srednia, nazwa_dzialu FROM pracownicy, organizacja WHERE (dzial=id_org) GROUP BY dzial HAVING AVG(zarobki)<28');
           echo("<table border=1>");
           echo("<th>DZIAŁ</th>");
-          echo("<td>NAZWA DZIAŁU</td>");
           echo("<th>ŚREDNIA</th>");
+          echo("<td>NAZWA DZIAŁU</td>");
               while($row = $result -> fetch_assoc()){
                   echo("<tr>");
-                      echo("<td>" .$row["dzial"]. "</td><td>" .$row["nazwa_dzial"]. "</td><td>" .$row["srednia"]. "</td>");
+                      echo("<td>" .$row["dzial"]. "</td><td>" .$row["srednia"]. "</td><td>" .$row["nazwa_dzialu"]. "</td>");
                   echo("</tr>");
               }
           echo("</table>");
   
   echo("<h3>2. Średnia Zarobków Mężczyzn W Poszczególnych Działach Większa Od 30</h3>");
       $conn = new mysqli("remotemysql.com","Rp4CxP6YkY","V0BMRFi2V3","Rp4CxP6YkY");
-      $result = $conn -> query('SELECT dzial, nazwa_dzial, AVG(zarobki) AS srednia FROM pracownicy, organizacja WHERE (dzial=id_org) AND (imie noy like "%a") GROUP BY dzial HAVING AVG(zarobki)<28');
+      $result = $conn -> query('SELECT dzial, AVG(zarobki) AS srednia, nazwa_dzialu FROM pracownicy, organizacja WHERE (dzial=id_org) AND (imie noy like "%a") GROUP BY dzial HAVING AVG(zarobki)<28');
           echo("<table border=1>");
           echo("<th>DZIAŁ</th>");
-          echo("<td>NAZWA DZIAŁU</td>");
           echo("<th>ŚREDNIA</th>");
+          echo("<td>NAZWA DZIAŁU</td>");
               while($row = $result -> fetch_assoc()){
                   echo("<tr>");
-                      echo("<td>" .$row["dzial"]. "</td><td>" .$row["nazwa_dzial"]. "</td><td>" .$row["srednia"]. "</td>");
+                      echo("<td>" .$row["dzial"]. "</td><td>" .$row["srednia"]. "</td><td>" .$row["nazwa_dzialu"]. "</td>");
                   echo("</tr>");
               }
           echo("</table>");
   
   echo("<h3>3. Ilość Pracowników W Poszczególnych Działach Większa Od 3</h3>");
       $conn = new mysqli("remotemysql.com","Rp4CxP6YkY","V0BMRFi2V3","Rp4CxP6YkY");
-      $result = $conn -> query('SELECT dzial, nazwa_dzial, COUNT(imie) AS ilosc FROM pracownicy, organizacja WHERE (dzial=id_org) GROUP BY dzial HAVING COUNT(imie)<3');
+      $result = $conn -> query('SELECT dzial, COUNT(imie) AS ilosc, nazwa_dzialu FROM pracownicy, organizacja WHERE (dzial=id_org) GROUP BY dzial HAVING COUNT(imie)<3');
           echo("<table border=1>");
           echo("<th>DZIAŁ</th>");
-          echo("<td>NAZWA DZIAŁU</td>");
           echo("<th>ILOŚĆ</th>");
+          echo("<td>NAZWA DZIAŁU</td>");
               while($row = $result -> fetch_assoc()){
                   echo("<tr>");
-                      echo("<td>" .$row["dzial"]. "</td><td>" .$row["nazwa_dzial"]. "</td><td>" .$row["srednia"]. "</td>");
+                      echo("<td>" .$row["dzial"]. "</td><td>" .$row["ilosc"]. "</td><td>" .$row["nazwa_dzialu"]. "</td>");
                   echo("</tr>");
               }
           echo("</table>");
