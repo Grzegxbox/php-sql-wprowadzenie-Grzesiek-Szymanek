077a21716b245c07b3c120ad802402da30a6fad2
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-11-08 20:35:13 +0100

    Update funkcjeAgregujace_klauzula_HAVING.php

--- funkcjeAgregujace_klauzula_HAVING.php
+++ funkcjeAgregujace_klauzula_HAVING.php
@@ -15,7 +15,7 @@
 <?php
   echo("<h3>1. Średnia Zarobków W Poszczególnych Działach Mniejsza Od 28</h3>");
       $conn = new mysqli("remotemysql.com","Rp4CxP6YkY","V0BMRFi2V3","Rp4CxP6YkY");
-      $result = $conn -> query('SELECT dzial, AVG(zarobki) AS srednia, nazwa_dzialu FROM pracownicy, organizacja WHERE (dzial=id_org) GROUP BY dzial HAVING AVG(zarobki)<28');
+      $result = $conn -> query('SELECT dzial, nazwa_dzialu, AVG(zarobki) AS srednia FROM pracownicy, organizacja WHERE (dzial=id_org) GROUP BY dzial HAVING AVG(zarobki)<28');
           echo("<table border=1>");
           echo("<th>DZIAŁ</th>");
           echo("<th>ŚREDNIA</th>");
