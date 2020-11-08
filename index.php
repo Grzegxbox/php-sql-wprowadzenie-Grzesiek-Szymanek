485ed853119ab81ff296a8b5c0967470e6c52ef5
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-11-08 15:30:38 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,12 +4,14 @@
   
 <link rel="stylesheet" href="style.css">
 <a class="mojlink0" href="https://github.com/SK-2019/php-sql-wprowadzenie-Grzesiek-Szymanek">Kliknij aby zobaczyć wszystko</a>
-  
-<div class="nav">
-  <a class="navigation_link0" href="https://nieumiem.herokuapp.com/">Strona Główna</a>
-  <a class="navigation_link1" href="pracownicy.php">Pracownicy</a>
-  <a class="navigation_link2" href="funkcjeAgregujace.php">Funkcje Agregujące</a>
-  <a class="navigation_link3" href="pracownicyiorganizacja.php">Pracownicy I Organizacja</a>
+
+<div class="navbar fixed-top navbar-expand-lg navbar-dark bg-inverse">
+  <div class="nav">
+    <a class="navigation_link0" href="https://nieumiem.herokuapp.com/">Strona Główna</a>
+    <a class="navigation_link1" href="pracownicy.php">Pracownicy</a>
+    <a class="navigation_link2" href="funkcjeAgregujace.php">Funkcje Agregujące</a>
+    <a class="navigation_link3" href="pracownicyiorganizacja.php">Pracownicy I Organizacja</a>
+  </div>
 </div>
   
 <h1>STRONA GŁÓWNA</h1>
