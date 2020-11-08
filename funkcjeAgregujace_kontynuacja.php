ff823f9202e65c03267e66946818c7783e561617
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-11-08 19:10:41 +0100

    Update funkcjeAgregujace_kontynuacja.php

--- funkcjeAgregujace_kontynuacja.php
+++ funkcjeAgregujace_kontynuacja.php
@@ -14,9 +14,21 @@
 
 
 <?php
-echo("<h3>1. Suma Wszystkich Pracowników</h3>");
+  echo("<h3>1. Suma Zarobków Wszystkich Pracowników</h3>");
+      $conn = new mysqli("remotemysql.com","Rp4CxP6YkY","V0BMRFi2V3","Rp4CxP6YkY");
+      $result = $conn -> query('SELECT SUM(zarobki) AS SUMA FROM pracownicy ');
+          echo("<table border=1>");
+          echo("<th>SUMA</th>");
+              while($row = $result -> fetch_assoc()){
+                  echo("<tr>");
+                      echo("<td>" .$row["SUMA"]. "</td>");
+                  echo("</tr>");
+              }
+          echo("</table>");
+  
+  echo("<h3>2. Suma Zarobków Wszystkich Kobiet</h3>");
     $conn = new mysqli("remotemysql.com","Rp4CxP6YkY","V0BMRFi2V3","Rp4CxP6YkY");
-    $result = $conn -> query('SELECT SUM(zarobki) AS SUMA FROM pracownicy ');
+    $result = $conn -> query('SELECT SUM(zarobki) AS SUMA FROM pracownicy WHERE (imie like "%a") ');
         echo("<table border=1>");
         echo("<th>SUMA</th>");
             while($row = $result -> fetch_assoc()){
@@ -25,6 +37,62 @@ echo("<h3>1. Suma Wszystkich Pracowników</h3>");
                 echo("</tr>");
             }
         echo("</table>");
+  
+   echo("<h3>3. Suma Zarobków Mężczyzn Z Działów 2 I 3</h3>");
+    $conn = new mysqli("remotemysql.com","Rp4CxP6YkY","V0BMRFi2V3","Rp4CxP6YkY");
+    $result = $conn -> query('SELECT SUM(zarobki) AS SUMA FROM pracownicy WHERE (imie not like "%a") AND (dzial=2 OR dzial=3) ');
+        echo("<table border=1>");
+        echo("<th>SUMA</th>");
+            while($row = $result -> fetch_assoc()){
+                echo("<tr>");
+                    echo("<td>" .$row["SUMA"]. "</td>");
+                echo("</tr>");
+            }
+        echo("</table>");
+  
+         echo("</table>");
+  
+   echo("<h3>4. Średnia Zarobków Wszystkich Mężczyzn</h3>");
+    $conn = new mysqli("remotemysql.com","Rp4CxP6YkY","V0BMRFi2V3","Rp4CxP6YkY");
+    $result = $conn -> query('SELECT AVG(zarobki) AS srednia FROM pracownicy WHERE (imie not like "%a") ');
+        echo("<table border=1>");
+        echo("<th>ŚREDNIA</th>");
+            while($row = $result -> fetch_assoc()){
+                echo("<tr>");
+                    echo("<td>" .$row["srednia"]. "</td>");
+                echo("</tr>");
+            }
+        echo("</table>");
+  
+    echo("<h3>5. Średnia Zarobków Pracowników W 4 Dziale</h3>");
+    $conn = new mysqli("remotemysql.com","Rp4CxP6YkY","V0BMRFi2V3","Rp4CxP6YkY");
+    $result = $conn -> query('SELECT AVG(zarobki) AS srednia FROM pracownicy WHERE dzial=4 ');
+        echo("<table border=1>");
+        echo("<th>DZIAŁ</th>");
+        echo("<th>ŚREDNIA</th>");
+            while($row = $result -> fetch_assoc()){
+                echo("<tr>");
+                    echo("<td>" .$row["dzial"]. "</td><td>" .$row["srednia"]. "</td>");
+                echo("</tr>");
+            }
+        echo("</table>");
+  
+    echo("<h3>6. Średnia Zarobków Wszystkich Mężczyzn</h3>");
+    $conn = new mysqli("remotemysql.com","Rp4CxP6YkY","V0BMRFi2V3","Rp4CxP6YkY");
+    $result = $conn -> query('SELECT AVG(zarobki) AS srednia FROM pracownicy WHERE (imie not like "%a") AND (dzial=2 OR dzial=4) ');
+        echo("<table border=1>");
+        echo("<th>ŚREDNIA</th>");
+            while($row = $result -> fetch_assoc()){
+                echo("<tr>");
+                    echo("<td>" .$row["srednia"]. "</td>");
+                echo("</tr>");
+            }
+        echo("</table>");
+  
+  
+  
+  
+  
 
 
 ?>
