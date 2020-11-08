012560f3797e9fb2353d62ddc250f44f22118397
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-11-08 18:40:08 +0100

    Update pracownicyiorganizacja_ograniczanie.php

--- pracownicyiorganizacja_ograniczanie.php
+++ pracownicyiorganizacja_ograniczanie.php
@@ -47,15 +47,17 @@ echo("<h3>2. Trzy Najlepsze Zarabiające Kobiety Z Działów 2 I 4</h3>");
 
 echo("<h3>3. Jeden Najstarszy Pracownik</h3>");
     $conn = new mysqli ("remotemysql.com","Rp4CxP6YkY","V0BMRFi2V3","Rp4CxP6YkY");
-    $result = $conn -> query('SELECT * FROM pracownicy, organizacja WHERE (dzial=id_org) GROUP BY data_rodzenia');
+    $result = $conn -> query('SELECT * FROM pracownicy, organizacja WHERE (dzial=id_org) ORDER BY data desc LIMIT 1');
             echo("<table border=1>");
             echo("<th>ID</th>");
             echo("<th>IMIĘ</th>");
             echo("<th>DZIAŁ</th>");
             echo("<th>ZAROBKI</th>");
+            echo("<th>NAZWA DZIAŁU</th>");
+            echo("<th>DATA URODZENIA</th>");
                 while($row = $result -> fetch_assoc()){
                     echo("<tr>");
-                        echo("<td>" .$row["id_pracownicy"]. "</td><td>" .$row["imie"]. "</td><td>" .$row["dzial"]. "</td><td>" .$row["zarobki"]. "</td>");
+                        echo("<td>" .$row["id_pracownicy"]. "</td><td>" .$row["imie"]. "</td><td>" .$row["dzial"]. "</td><td>" .$row["zarobki"]. "</td><td>" .$row["nazwa_dzial"]. "</td><td>" .$row["data_urodzenia"]. "</td>");
                     echo("</tr>");
                 }
             echo("</table>");
