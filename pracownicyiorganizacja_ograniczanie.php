fcf33baa2502bf71a6f3e7726c209b054c09ed8f
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-11-08 18:09:09 +0100

    Update pracownicyiorganizacja_ograniczanie.php

--- pracownicyiorganizacja_ograniczanie.php
+++ pracownicyiorganizacja_ograniczanie.php
@@ -1,1 +1,14 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+  
+<link rel="stylesheet" href="style.css">
+<a class="mojlink0" href="https://github.com/SK-2019/php-sql-wprowadzenie-Grzesiek-Szymanek">Kliknij aby zobaczyć wszystko</a>
+  
+<div class="nav">
+  <a class="navigation_link3" href="pracownicyiorganizacja.php">Pracownicy I Organizacja</a>
+</div>
+  
+<h1>WYKORZYSTANIE FUNKCJI 'LIMIT'</h1>
+ 
+<?php
