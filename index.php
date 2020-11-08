009b7fb108b029133181490b84959b944959ac77
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-11-08 20:05:56 +0100

    Update index.php

--- index.php
+++ index.php
@@ -9,8 +9,8 @@
   <div class="nav">
     <a class="navigation_link0" href="https://nieumiem.herokuapp.com/">Strona Główna</a>
     <a class="navigation_link1" href="pracownicy.php">Pracownicy</a>
-    <a class="navigation_link2" href="funkcjeAgregujace.php">Funkcje Agregujące</a>
-    <a class="navigation_link3" href="pracownicyiorganizacja.php">Pracownicy I Organizacja</a>
+    <a class="navigation_link1" href="funkcjeAgregujace.php">Funkcje Agregujące</a>
+    <a class="navigation_link1" href="pracownicyiorganizacja.php">Pracownicy I Organizacja</a>
   </div>
 
   
