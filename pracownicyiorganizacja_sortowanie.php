c08f2c569fe399a7e33cd0c4dab783837b7a851f
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-11-09 19:35:36 +0100

    Update pracownicyiorganizacja_sortowanie.php

--- pracownicyiorganizacja_sortowanie.php
+++ pracownicyiorganizacja_sortowanie.php
@@ -60,7 +60,7 @@
             }
         echo("</table>");
   
-  echo("<h3>4. SELECT * FROM pracownicy, organizacja WHERE (dzial=id_org) AND (imie like "%a") ORDER BY zarobki asc</h3>");
+  echo("<h3>4. SELECT * FROM pracownicy, organizacja WHERE (dzial=id_org) AND (imie like '%a') ORDER BY zarobki asc</h3>");
     $conn = new mysqli("remotemysql.com","Rp4CxP6YkY","V0BMRFi2V3","Rp4CxP6YkY");
     $result = $conn -> query('SELECT * FROM pracownicy, organizacja WHERE (dzial=id_org) AND (imie like "%a") ORDER BY zarobki asc');
         echo("<table border=1>");
