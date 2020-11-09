10051ec24d14f3d9c229fe2afae424455adfcd91
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-11-09 19:16:04 +0100

    Update connect.php

--- connect.php
+++ connect.php
@@ -4,7 +4,7 @@ $servername = "remotemysql.com";
 $username = "Rp4CxP6YkY";
 $password = "V0BMRFi2V3";
 $dbname = " Rp4CxP6YkY";
-// Create connection
+// Create connection(łącze
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
