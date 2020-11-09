29e50ba43e013b1118dd8e92da0d127cb87c9f7d
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-11-09 19:18:22 +0100

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,15 +1,15 @@
 <?php
 
-// jak by co to nie mam takich nazw użytkownika, haseł i nazwy serwera
+// Add your data (jak by co to nie mam takich nazw użytkownika, haseł i nazwy serwera)
 $servername = "remotemysql.com";
 $username = "Rp4CxP6YkY";
 $password = "V0BMRFi2V3";
 $dbname = " Rp4CxP6YkY"; 
 
-// Create connection(łącze się z moją bazą danych)
+// Create connection (łącze się z moją bazą danych)
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection(sprawdzam połączenie z bazą danych)
+// Check connection (sprawdzam połączenie z bazą danych)
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
