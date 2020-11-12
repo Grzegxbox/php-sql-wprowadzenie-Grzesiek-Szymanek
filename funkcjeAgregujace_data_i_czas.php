f909ea5d42c462e636e43a648361d91b66e580d9
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-11-12 21:18:16 +0100

    Update funkcjeAgregujace_data_i_czas.php

--- funkcjeAgregujace_data_i_czas.php
+++ funkcjeAgregujace_data_i_czas.php
@@ -57,16 +57,16 @@
               }
           echo("</table>");
   
-  echo("<h3>4. SELECT SUM(YEAR(CURDATE()) - YEAR(data_urodzenia)) AS suma FROM pracownicy, organizacja WHERE id_org=dzial and (nazwa_dzial='handel')</h3>");
+  echo("<h3>4. SELECT nazwa_dzial, SUM(YEAR(CURDATE()) - YEAR(data_urodzenia)) AS suma FROM pracownicy, organizacja WHERE (id_org=dzial) and (nazwa_dzial='handel')</h3>");
       $conn = new mysqli("remotemysql.com","Rp4CxP6YkY","V0BMRFi2V3","Rp4CxP6YkY");
-      $result = $conn -> query('SELECT dzial, nazwa_dzial, SUM(YEAR(CURDATE()) - YEAR(data_urodzenia)) AS suma FROM pracownicy, organizacja WHERE (id_org=dzial) and (nazwa_dzial="handel")');
+      $result = $conn -> query('SELECT nazwa_dzial, SUM(YEAR(CURDATE()) - YEAR(data_urodzenia)) AS suma FROM pracownicy, organizacja WHERE (id_org=dzial) and (nazwa_dzial="handel")');
           echo("<table border=1>");
           echo("<th>DZIAŁ</th>");
           echo("<th>NAZWA DZIAŁU</th>");
           echo("<th>SUMA LAT</th>");
               while($row = $result -> fetch_assoc()){
                   echo("<tr>");
-                      echo("<td>" .$row["dzial"]. "</td><td>" .$row["nazwa_dzial"]. "</td><td>" .$row["suma"]. "</td>");
+                      echo("<td>" .$row["nazwa_dzial"]. "</td><td>" .$row["suma"]. "</td>");
                   echo("</tr>");
               }
           echo("</table>");
