2978a2b77f6d048ba0a298ad87cf5f999b4f5211
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-11-12 21:26:46 +0100

    Update funkcjeAgregujace_data_i_czas.php

--- funkcjeAgregujace_data_i_czas.php
+++ funkcjeAgregujace_data_i_czas.php
@@ -71,6 +71,21 @@
               }
           echo("</table>");
   
+  echo("<h3>5. SELECT SUM(YEAR(CURDATE()) - YEAR(data_urodzenia)) as SumaWiekuKobiet from pracownicy WHERE imie LIKE '%a';</h3>");
+      $conn = new mysqli("remotemysql.com","Rp4CxP6YkY","V0BMRFi2V3","Rp4CxP6YkY");
+      $result = $conn -> query('SELECT SUM(YEAR(CURDATE()) - YEAR(data_urodzenia)) AS suma,  if(imie LIKE "a", 'Kobiety','Mężczyźni') as 'plec' FROM pracownicy WHERE (imie LIKE "%a")');
+          echo("<table border=1>");
+          echo("<th>DZIAŁ</th>");
+          echo("<th>NAZWA DZIAŁU</th>");
+          echo("<th>SUMA LAT</th>");
+              while($row = $result -> fetch_assoc()){
+                  echo("<tr>");
+                      echo("<td>" .$row["dzial"]. "</td><td>" .$row["nazwa_dzial"]. "</td><td>" .$row["suma"]. "</td>");
+                  echo("</tr>");
+              }
+          echo("</table>");
+  
+  
  
 ?>
 
