3c9943acbc710f310221a476f5ad8b8b80e5304f
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-11-12 21:38:20 +0100

    Update funkcjeAgregujace_data_i_czas.php

--- funkcjeAgregujace_data_i_czas.php
+++ funkcjeAgregujace_data_i_czas.php
@@ -97,6 +97,20 @@
               }
           echo("</table>");
   
+  echo("<h3>7. SELECT dzial, nazwa_dzial, AVG(YEAR(CURDATE()) - YEAR(data_urodzenia)) AS suma FROM pracownicy, organizacja WHERE (id_org=dzial) GROUP BY dzial</h3>");
+      $conn = new mysqli("remotemysql.com","Rp4CxP6YkY","V0BMRFi2V3","Rp4CxP6YkY");
+      $result = $conn -> query('SELECT dzial, nazwa_dzial, AVG(YEAR(CURDATE()) - YEAR(data_urodzenia)) AS suma FROM pracownicy, organizacja WHERE (id_org=dzial) GROUP BY dzial');
+          echo("<table border=1>");
+          echo("<th>DZIAŁ</th>");
+          echo("<th>NAZWA DZIAŁU</th>");
+          echo("<th>ŚREDNIA WIEKU</th>");
+              while($row = $result -> fetch_assoc()){
+                  echo("<tr>");
+                      echo("<td>" .$row["dzial"]. "</td><td>" .$row["nazwa_dzial"]. "</td><td>" .$row["suma"]. "</td>");
+                  echo("</tr>");
+              }
+          echo("</table>");
+  
   
  
 ?>
