5fbc70d7d19599a8489b20f42c1b62e0b052b356
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-11-13 09:02:55 +0100

    Update funkcjeAgregujace_data_i_czas.php

--- funkcjeAgregujace_data_i_czas.php
+++ funkcjeAgregujace_data_i_czas.php
@@ -184,6 +184,23 @@
               }
           echo("</table>");
   
+  echo("<h3>13. SELECT * FROM pracownicy, organizacja WHERE (dzial=id_org) AND (imie NOT LIKE '%a') ORDER BY data_urodzenia ASC LIMIT 1</h3>");
+      $conn = new mysqli("remotemysql.com","Rp4CxP6YkY","V0BMRFi2V3","Rp4CxP6YkY");
+      $result = $conn -> query('SELECT * FROM pracownicy, organizacja WHERE (dzial=id_org) AND (imie NOT LIKE "%a") ORDER BY data_urodzenia ASC LIMIT 1');
+          echo("<table border=1>");
+          echo("<th>ID</th>");
+          echo("<th>IMIĘ</th>");
+          echo("<th>DZIAŁ</th>");
+          echo("<th>ZAROBKI</th>");
+          echo("<th>NAZWA DZIAŁU</th>");
+          echo("<th>DATA URODZENIA</th>");
+              while($row = $result -> fetch_assoc()){
+                  echo("<tr>");
+                      echo("<td>" .$row["id_pracownicy"]. "</td><td>" .$row["imie"]. "</td><td>" .$row["dzial"]. "</td><td>" .$row["zarobki"]. "</td><td>" .$row["nazwa_dzial"]. "</td><td>" .$row["data_urodzenia"]. "</td>");
+                  echo("</tr>");
+              }
+          echo("</table>");
+  
   
 
  
