47beb610c79063d9fc4c921cfff9ef0b982c2daa
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-11-13 13:56:09 +0100

    Update funkcjeAgregujace_formatowanie_dat.php

--- funkcjeAgregujace_formatowanie_dat.php
+++ funkcjeAgregujace_formatowanie_dat.php
@@ -45,6 +45,22 @@ echo("<h3>2. SET lc_time_names = 'pl_PL'; SELECT DATE_FORMAT(CURDATE(), '%W') AS
             }
         echo("</table>");
   
+   echo("<h3>2. SELECT *, DATE_FORMAT(data_urodzenia,'%w-%M-%Y') AS dni FROM pracownicy</h3>");
+  $conn= new mysqli("remotemysql.com","Rp4CxP6YkY","V0BMRFi2V3","Rp4CxP6YkY");
+    $result=$conn->query('SELECT *, DATE_FORMAT(data_urodzenia,"%w-%M-%Y") AS dni FROM pracownicy');
+        echo("<table border=2>");
+        echo("<th>ID</th>");
+        echo("<th>IMIĘ</th>");
+        echo("<th>DZIAŁ</th>");
+        echo("<th>ZAROBKI</th>");
+        echo("<th>DATA URODZENIA</th>");
+            while($row=$result->fetch_assoc()){
+                echo("<tr>");
+                    echo("<td>" .$row["id_pracownicy"]. "</td><td>" .$row["imie"]. "</td><td>" .$row["dzial"]. "</td><td>" .$row["zarobki"]. "</td><td>" .$row["dni"]. "</td>");
+                echo("</tr>");
+            }
+        echo("</table>");
+  
   
   
   
