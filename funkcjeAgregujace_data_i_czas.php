5f1132099ad30c2a963609aa5d0b7d9e5451730a
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-11-13 10:39:37 +0100

    Update funkcjeAgregujace_data_i_czas.php

--- funkcjeAgregujace_data_i_czas.php
+++ funkcjeAgregujace_data_i_czas.php
@@ -159,7 +159,7 @@
   
   echo("<h3>11. SELECT imie, dzial, nazwa_dzial, MIN(YEAR(CURDATE()) - YEAR(data_urodzenia)) AS mini  FROM pracownicy, organizacja WHERE (id_org=dzial) AND (nazwa_dzial='handel' or nazwa_dzial='serwis') GROUP BY dzial</h3>");
       $conn = new mysqli("remotemysql.com","Rp4CxP6YkY","V0BMRFi2V3","Rp4CxP6YkY");
-      $result = $conn -> query('SELECT imie, dzial, nazwa_dzial, MIN(YEAR(CURDATE()) - YEAR(data_urodzenia)) AS mini  FROM pracownicy, organizacja WHERE (id_org=dzial) AND (nazwa_dzial="handel" or nazwa_dzial="serwis") GROUP BY dzial');
+      $result = $conn -> query('SELECT imie, dzial, nazwa_dzial, MIN(YEAR(CURDATE()) - YEAR(data_urodzenia)) AS mini  FROM pracownicy, organizacja WHERE (id_org=dzial) AND (nazwa_dzial="handel" OR dzial=2 OR imie="dagmara") AND (nazwa_dzial="serwis" OR dzial=1 OR imie="patrycja") GROUP BY dzial');
           echo("<table border=1>");
           echo("<th>IMIĘ</th>");
           echo("<th>DZIAŁ</th>");
