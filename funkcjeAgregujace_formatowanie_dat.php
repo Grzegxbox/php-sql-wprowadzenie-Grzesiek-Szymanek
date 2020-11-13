01aef8ce0ae08b8003a9a3068ec53e29dbb5a3c1
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-11-13 16:05:48 +0100

    Update funkcjeAgregujace_formatowanie_dat.php

--- funkcjeAgregujace_formatowanie_dat.php
+++ funkcjeAgregujace_formatowanie_dat.php
@@ -105,12 +105,12 @@ echo("<h3>6. SELECT imie, DATEDIFF(CURDATE(),data_urodzenia) AS dd, DATEDIFF(CUR
             }
         echo("</table>");
   
-echo("<h3>7. SELECT imie, DATE_FORMAT('data_urodzenia', '%Y-%M-%W') AS dzur FROM pracownicy WHERE (imie='Grzesiek')</h3>");
+echo("<h3>7. SELECT imie, DATE_FORMAT('2003-07-030', '%j') AS dzur FROM pracownicy WHERE (imie='Grzesiek')</h3>");
   $conn= new mysqli("remotemysql.com","Rp4CxP6YkY","V0BMRFi2V3","Rp4CxP6YkY");
-    $result=$conn->query('SELECT imie, DATE_FORMAT("data_urodzenia", "%Y-%M-%W") AS dzur FROM pracownicy WHERE (imie="Grzesiek")');
+    $result=$conn->query('SELECT imie, DATE_FORMAT("2003-07-030", "%j") AS dzur FROM pracownicy WHERE (imie="Grzesiek")');
         echo("<table border=2>");
         echo("<th>IMIĘ</th>");
-        echo("<th>DZIEŃ NARODZIN</th>");
+        echo("<th>DZIEŃ W ROKU</th>");
             while($row=$result->fetch_assoc()){
                 echo("<tr>");
                     echo("<td>" .$row["imie"]. "</td><td>" .$row["dzur"]. "</td>");
