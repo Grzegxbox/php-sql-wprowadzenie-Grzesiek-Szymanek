5b0bec5a4113095078629332fad16fad747cb4a3
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-11-13 12:28:02 +0100

    Update funkcjeAgregujace_formatowanie_dat.php

--- funkcjeAgregujace_formatowanie_dat.php
+++ funkcjeAgregujace_formatowanie_dat.php
@@ -1,1 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+  
+<link rel="stylesheet" href="style.css">
+<a class="mojlink0" href="https://github.com/SK-2019/php-sql-wprowadzenie-Grzesiek-Szymanek">Kliknij aby zobaczyć wszystko</a>
+  
+<div class="nav">
+  <a class="navigation_link1" href="funkcjeAgregujace_data_i_czas.php">FUNKCJA AGREGUJĄCA - DATA I CZAS</a>
+</div>
+  
+  </head>
+  <hr></hr>
+<body>
+  
+<h1>FUNKCJA AGREGUJĄCA - DATA I CZAS - FORMATOWANIE DAT</h1>
+  
+  <?php
+  
+  
+  
+  
+  
+  
+  
+  ?>
+  
+  </body>
+</html>
 
+
+  
