5e3972fb4c036dbe0363010356499bfaef067f80
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-11-13 14:12:08 +0100

    Update funkcjeAgregujace_formatowanie_dat.php

--- funkcjeAgregujace_formatowanie_dat.php
+++ funkcjeAgregujace_formatowanie_dat.php
@@ -61,7 +61,7 @@ echo("<h3>2. SET lc_time_names = 'pl_PL'; SELECT DATE_FORMAT(CURDATE(), '%W') AS
             }
         echo("</table>");
   
-echo("<h3>4. SELECT curtime(4) AS akttime</h3>");
+echo("<h3>4. SELECT datetimeoffset(4) AS akttime</h3>");
   $conn= new mysqli("remotemysql.com","Rp4CxP6YkY","V0BMRFi2V3","Rp4CxP6YkY");
     $result=$conn->query('SELECT curtime(4) AS akttime');
         echo("<table border=2>");
