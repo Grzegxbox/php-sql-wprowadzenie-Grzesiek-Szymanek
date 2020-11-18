6fb5433f023b9615b7f4d9ea195161e05700c045
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-11-18 14:37:26 +0100

    Update connect.php

--- connect.php
+++ connect.php
@@ -14,7 +14,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "INSERT INTO ";
+$sql = "INSERT INTO `pracownicy`(`id_pracownicy`, `imie`, `dzial`, `zarobki`, `data_urodzenia`) VALUES ('20','Filip','1','150','2003-07-30')";
 
 if ($conn->query($sql) === TRUE) {
   echo "New record created successfully";
