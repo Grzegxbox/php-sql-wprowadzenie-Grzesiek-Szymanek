655533f72dcdc77b9ad0b380f6a7c7c257a54b9f
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-11-18 14:44:00 +0100

    Update strona.php

--- strona.php
+++ strona.php
@@ -1,8 +1,10 @@
 <?php
 
-// echo("jesteś na strona.php");
+
 echo("<h3>Twoje Dane</h3>");
 echo("<li>($_POST['imie'])</li>");
+echo("<li>($_POST['nazwisko'])</li>");
+echo("<li>($_POST['miejscowosc'])</li>");
 
 
 ?>
