aa52094fac395b649c22194d298168eb81244bb6
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-11-20 12:47:57 +0100

    Update strona.php

--- strona.php
+++ strona.php
@@ -20,21 +20,32 @@ echo($_POST['zarobki']);
 
 echo("<h3>DATA URODZENIA</h3>");
 echo($_POST['data_urodzenia']);
+    
 
+$servername = "remotemysql.com";
+$username = "Rp4CxP6YkY";
+$password = "V0BMRFi2V3";
+$dbname = " Rp4CxP6YkY"; 
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
     
 require_once("connect.php");
-$sql = "INSERT INTO pracownicy ('id_pracownicy', 'imie', 'dzial', 'zarobki', 'data_rodzenia')
-VALUES ('NULL', 'imie', 'dzial', 'zarobki', 'data_urodzenia')";
+$sql = "INSERT INTO Pracownik (null, imie, dzial, zarobki) 
+       VALUES (null,'Bartosz', 1, 27,'1969-01-17')";
 
 if ($conn->query($sql) === TRUE) {
   echo "New record created successfully :)";
 } else {
   echo "Error: " . $sql . "<br>" . $conn->error;
 }
+    
 
 $conn->close();
 
 ?>
-
+    
 </body>
 </html>
