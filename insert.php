8f51b49d545934930f984afbc2c85077e3e0b084
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-11-20 13:51:38 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -29,7 +29,8 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "INSERT INTO pracownicy (id_pracownicy, imie, dzial, zarobki, data_urodzenia)";
+$sql = "INSERT INTO pracownicy (id_pracownicy, imie, dzial, zarobki, data_urodzenia)
+VALUES(NULL,'".$_POST['imie']."', '".$_POST['dzial']."', '".$_POST['zarobki']."', '".$_POST['data_ur']."')";
 
 if ($conn->query($sql) === TRUE) {
   echo "New record created successfully :)";
