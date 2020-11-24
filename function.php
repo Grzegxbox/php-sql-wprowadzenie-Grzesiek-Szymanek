e0c57ed0ab2c2476f155d0aadb1de14afdfb6393
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-11-24 13:15:27 +0100

    Update function.php

--- function.php
+++ function.php
@@ -3,14 +3,14 @@
 echo("<h1>Funkcje</h1>");
 
 function liczby($liczba){
-    echo("<li>Początek funkcji</li>");
+    echo("<li>Początek funkcji ");
     echo("<li>START ".$liczba);
-    for($i='1';$i<$liczba;$i++){
+    for($i=1;$i<$liczba;$i++){
         echo("<li>Kolejna liczba: ".$i);
     }
 }
 
-liczba('15')
+liczby('15')
 
 
 ?>
