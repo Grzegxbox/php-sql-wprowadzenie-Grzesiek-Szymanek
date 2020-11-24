6f1b34d7eaac78b6dc12cf614d2955d6f58bf6d2
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-11-24 13:09:07 +0100

    Update function.php

--- function.php
+++ function.php
@@ -2,6 +2,15 @@
     
 echo("<h1>Funkcje</h1>");
 
+function literki($litera){
+    echo("<li>Początek funkcji</li>");
+    echo("<li>START ".$litera);
+    for($i='a';$i<$litera;$i++){
+        echo("<li>Kolejna literka: ".$i);
+    }
+}
+
+literki('abcdefghijklmnoprstuwyz')
 
 
 ?>
