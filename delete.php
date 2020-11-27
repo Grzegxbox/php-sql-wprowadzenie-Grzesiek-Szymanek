8a39b5bb94dd6dbf8b081a19c8c434a886f0b19c
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-11-27 08:58:40 +0100

    Update delete.php

--- delete.php
+++ delete.php
@@ -6,21 +6,23 @@
   <meta name="viewport" content="width=device-width">
   <title>Dane do Bazy</title>
 </head>
-
+<body>
 
 <?php
 
-echo("<h1>id: ".$_POST['id']."</h1>");
+echo("<h1>ID: ".$_POST['id']."</h1>");
   require_once('connect.php');
 
 $sql = "DELETE FROM pracownicy WHERE id_pracownicy=('".$_POST['id']."')";
 
-if($conn->query($sql)==TRUE){
-  echo("<h1 class='ja'>PRACOWNIK ZOSTAŁ USUNIĘTY</h1>");
+if ($conn->query($sql) === TRUE){
+  echo("PRACOWNIK ZOSTAŁ USUNIĘTY");
   } else  {
-  echo("<h1 class='ja'>'ERROR: ' .$sql. '<br>' .$conn->error</h1>");
+  echo('ERROR: ' .$sql. '<br>' .$conn->error);
   }
+  $conn->close();
 
 ?>
 
+  </body>
 </html>
