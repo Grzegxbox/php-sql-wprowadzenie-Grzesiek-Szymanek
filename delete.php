b8c2e34a8ce70c06a8108fc8718bfb2caee09c13
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-11-27 09:03:38 +0100

    Update delete.php

--- delete.php
+++ delete.php
@@ -9,9 +9,19 @@
 <body>
 
 <?php
-
+  
 echo("<h1>ID: ".$_POST['id']."</h1>");
-  require_once('connect.php');
+  
+$servername = "remotemysql.com";
+$username = "Rp4CxP6YkY";
+$password = "V0BMRFi2V3";
+$dbname = "Rp4CxP6YkY";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
 
 $sql = "DELETE FROM pracownicy WHERE id_pracownicy=('".$_POST['id']."')";
 
