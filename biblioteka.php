af32b543b11c2a8a7f5a7acb94e82f088f4ed7fd
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-12-02 17:03:19 +0100

    Update biblioteka.php

--- biblioteka.php
+++ biblioteka.php
@@ -1,3 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <a class="navigation_link1" href="https://wprowadzenie.herokuapp.com/">Strona Główna</a>
+  <hr></hr>
+  <link rel="stylesheet" href="style.css">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+</head>
+
+
 <?php
 
 echo("<h1>DZIAŁA?</h1>");
