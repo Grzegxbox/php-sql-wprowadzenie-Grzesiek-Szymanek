9a17c7778fb68bf1f333eb50ab5ff3c6d1be3c2b
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-12-02 18:33:15 +0100

    Update function.php

--- function.php
+++ function.php
@@ -20,9 +20,9 @@
 
 function liczby($liczba){
     echo("<h2>Początek funkcji ");
-    echo("<h2>START ".$liczba);
+    echo("<h2>Napisz Liczby Do ".$liczba);
     for($i=1;$i<$liczba;$i++){
-        echo("<h2>Kolejna liczba: ".$i);
+        echo("<h3>Kolejna liczba: ".$i);
     }
 }
 
