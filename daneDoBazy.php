f455dbe274993d3db585789258c2bec0c2e9efb8
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-12-02 17:12:48 +0100

    Update daneDoBazy.php

--- daneDoBazy.php
+++ daneDoBazy.php
@@ -3,8 +3,8 @@
 
 <head>
   <a class="mojlink0" href="https://github.com/SK-2019/php-sql-wprowadzenie-Grzesiek-Szymanek">Kliknij aby zobaczyć wszystko</a>
-  <hr></hr>
   <a class="navigation_link1" href="https://wprowadzenie.herokuapp.com/">Strona Główna</a>
+    <hr></hr>
   <link rel="stylesheet" href="style.css">
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width">
