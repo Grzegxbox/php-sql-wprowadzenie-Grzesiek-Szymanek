73aeeec23c4fb308fe37e6aecd7691b0f8988c90
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-12-02 17:17:14 +0100

    Update biblioteka.php

--- biblioteka.php
+++ biblioteka.php
@@ -2,11 +2,14 @@
 <html>
 
 <head>
-  <a class="navigation_link1" href="https://wprowadzenie.herokuapp.com/">Strona Główna</a>
-  <hr></hr>
-  <link rel="stylesheet" href="style.css">
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width">
+ <link rel="stylesheet" href="style.css">
+ <a class="mojlink0" href="https://github.com/SK-2019/php-sql-wprowadzenie-Grzesiek-Szymanek">Kliknij aby zobaczyć wszystko</a>
+  <div class="nav">
+    <a class="navigation_link1" href="https://wprowadzenie.herokuapp.com/">Strona Główna</a>
+  </div>
+ <hr></hr>
+ <meta charset="utf-8">
+ <meta name="viewport" content="width=device-width">
 </head>
 
 
