fbd437b2ff3cdf914695ba0384fbc0899d2e856d
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-12-02 18:42:55 +0100

    Update funkcjeAgregujace.php

--- funkcjeAgregujace.php
+++ funkcjeAgregujace.php
@@ -7,12 +7,13 @@
 <a class="mojlink0" href="https://github.com/SK-2019/php-sql-wprowadzenie-Grzesiek-Szymanek">Kliknij aby zobaczyć wszystko</a>
   
 <div class="nav">
-  <a class="navigation_link1" href="https://wprowadzenie.herokuapp.com/">Strona Główna</a>
-  <a class="navigation_link1" href="daneDoBazy.php">Formularz</a>
-  <a class="navigation_link1" href="pracownicy.php">Pracownicy</a>
-  <a class="navigation_link1" href="funkcjeAgregujace.php">Funkcje Agregujące</a>
-  <a class="navigation_link1" href="pracownicyiorganizacja.php">Pracownicy I Organizacja</a>
-  <a class="navigation_link1" href="biblioteka.php">Biblioteka</a>
+    <a class="navigation_link1" href="https://wprowadzenie.herokuapp.com/">Strona Główna</a>
+    <a class="navigation_link1" href="function.php">Funkcja</a>
+    <a class="navigation_link1" href="daneDoBazy.php">Formularz</a>
+    <a class="navigation_link1" href="pracownicy.php">Pracownicy</a>
+    <a class="navigation_link1" href="funkcjeAgregujace.php">Funkcje Agregujące</a>
+    <a class="navigation_link1" href="pracownicyiorganizacja.php">Pracownicy I Organizacja</a>
+    <a class="navigation_link1" href="biblioteka.php">Biblioteka</a>
 </div>
   
   </head>
