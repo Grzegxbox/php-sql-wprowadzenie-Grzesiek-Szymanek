7473b6bde6f63e5761ba3cef5124875e6d335c06
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-12-04 15:11:52 +0100

    Update biblioteka.php

--- biblioteka.php
+++ biblioteka.php
@@ -76,10 +76,10 @@ echo("<h3>3. SELECT * FROM biblAutor, biblTytul, biblAutor_biblTytul WHERE (bibl
 //           }
 //        echo("</select>");
 
-echo("<h3 style='margin-top: -1000px; margin-left: 800px;'>Wybierz Książkę I Autora Do Wypożyczenia</h3>");
+echo("<h3 style='margin-top: -1000px; margin-left: 900px;'>Wybierz Książkę I Autora Do Wypożyczenia</h3>");
     $conn= new mysqli("remotemysql.com","Rp4CxP6YkY","V0BMRFi2V3","Rp4CxP6YkY");
     $result=$conn->query('SELECT * FROM biblAutor');
-       echo("<select name='tytul' id='tytul' style='margin-left: 800px;'>");
+       echo("<select name='tytul' id='tytul' style='margin-left: 900px;'>");
           while($row=$result->fetch_assoc()){ 
             echo("<option value=".$row['id'].">".$row['autor']."</option>");
           }
