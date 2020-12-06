d053684a42368178db67a151de25005e2edf8433
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-12-06 20:38:19 +0100

    Update biblioteka.php

--- biblioteka.php
+++ biblioteka.php
@@ -2,7 +2,6 @@
 <html>
  
  
- 
 <head>
  <link rel="stylesheet" href="style.css">
  <a class="mojlink0" href="https://github.com/SK-2019/php-sql-wprowadzenie-Grzesiek-Szymanek">Kliknij aby zobaczyć wszystko</a>
