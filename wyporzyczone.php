b7693fa4271c4c89ce59b31b53b10f7de104723c
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-12-07 09:22:27 +0100

    Update wyporzyczone.php

--- wyporzyczone.php
+++ wyporzyczone.php
@@ -1,3 +1,16 @@
+<!DOCTYPE html>
+<html>
+ 
+<head>
+ <link rel="stylesheet" href="style.css">
+ <a class="mojlink0" href="https://github.com/SK-2019/php-sql-wprowadzenie-Grzesiek-Szymanek">Kliknij aby zobaczyć wszystko</a>
+  <div class="nav">
+    <a class="navigation_link1" href="https://wprowadzenie.herokuapp.com/">Strona Główna</a>
+  </div>
+ <hr></hr>
+ <meta charset="utf-8">
+ <meta name="viewport" content="width=device-width">
+
 <?php
 
 echo("<h1>Tutaj będą książki wypożyczone:)</h1>");
