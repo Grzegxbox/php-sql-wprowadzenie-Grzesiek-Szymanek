e4a8ccc30fce11dc22056e6ab52680fd69f47fa2
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-12-07 09:10:38 +0100

    Update pracownicyiorganizacja_ograniczanie.php

--- pracownicyiorganizacja_ograniczanie.php
+++ pracownicyiorganizacja_ograniczanie.php
@@ -18,7 +18,7 @@
  
 <?php
   
-echo("<h3>1. SELECT * FROM pracownicy, organizacja WHERE (dzial=id_org) AND (dzial=4) ORDER BY zarobki desc LIMIT 2</h3>");
+echo("<h3 style='margin-top: 70px;'>1. SELECT * FROM pracownicy, organizacja WHERE (dzial=id_org) AND (dzial=4) ORDER BY zarobki desc LIMIT 2</h3>");
     $conn= new mysqli("remotemysql.com","Rp4CxP6YkY","V0BMRFi2V3","Rp4CxP6YkY");
     $result=$conn->query('SELECT * FROM pracownicy, organizacja WHERE (dzial=id_org) AND (dzial=4) ORDER BY zarobki desc LIMIT 2');
         echo("<table border=2>");
