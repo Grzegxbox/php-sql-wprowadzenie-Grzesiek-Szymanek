1f8ccc5bf4df9ff19c25bfad2fe1ebabbf527a09
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-12-07 08:53:30 +0100

    Update funkcjeAgregujace_kontynuacja.php

--- funkcjeAgregujace_kontynuacja.php
+++ funkcjeAgregujace_kontynuacja.php
@@ -18,7 +18,7 @@
 
 <?php
   
-  echo("<h3>1. SELECT SUM(zarobki) AS SUMA FROM pracownicy</h3>");
+  echo("<h3 style='margin-top: 70px;'>1. SELECT SUM(zarobki) AS SUMA FROM pracownicy</h3>");
       $conn = new mysqli("remotemysql.com","Rp4CxP6YkY","V0BMRFi2V3","Rp4CxP6YkY");
       $result = $conn -> query('SELECT SUM(zarobki) AS SUMA FROM pracownicy');
           echo("<table border=1>");
