ba97080aad75e2caae416f7ee24b7ae2d063248a
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-12-07 08:55:17 +0100

    Update index.php

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
   
 <?php
   
-  echo("<h3>0. SELECT * FROM pracownicy</h3>");
+  echo("<h3 style='margin-top: 70px;'>0. SELECT * FROM pracownicy</h3>");
     $conn= new mysqli("remotemysql.com","Rp4CxP6YkY","V0BMRFi2V3","Rp4CxP6YkY");
     $result=$conn->query('SELECT * FROM pracownicy');
         echo("<table border=2>");
