bce6b2ebd11c8e57bbf1a86abd3da39589df643c
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-12-07 09:01:35 +0100

    Update funkcjeAgregujace_klauzula_HAVING.php

--- funkcjeAgregujace_klauzula_HAVING.php
+++ funkcjeAgregujace_klauzula_HAVING.php
@@ -18,7 +18,7 @@
 
 <?php
   
-  echo("<h3>1. SELECT dzial, nazwa_dzial, SUM(zarobki) AS suma,  FROM pracownicy, organizacja WHERE dzial=id_org GROUP BY dzial HAVING sum(zarobki) > 128</h3>");
+  echo("<h3 style='margin-top: 70px;'>1. SELECT dzial, nazwa_dzial, SUM(zarobki) AS suma,  FROM pracownicy, organizacja WHERE dzial=id_org GROUP BY dzial HAVING sum(zarobki) > 128</h3>");
       $conn = new mysqli("remotemysql.com","Rp4CxP6YkY","V0BMRFi2V3","Rp4CxP6YkY");
       $result = $conn -> query('SELECT dzial, nazwa_dzial, SUM(zarobki) AS suma  FROM pracownicy, organizacja WHERE dzial=id_org GROUP BY dzial HAVING sum(zarobki) > 128');
           echo("<table border=1>");
