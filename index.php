200ec81ed96048b898111175d54eeda64c56087a
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-12-11 08:37:16 +0100

    Update index.php

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
      <div class="container">
       <div class="item colorRed">
         banner
-        <h2 class="tltle">Grzesiek Szymanek</h2>
+        <h1 class="tltle">Grzesiek Szymanek</h1>
       </div>
       <div class="item colorBlue"></div>
       <div class="item colorGreen"></div>
