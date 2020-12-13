468a5101f27709ce557f3489e9eed6dbc5fc8171
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-12-13 18:02:15 +0100

    Update funkcjeAgregujace.php

--- funkcjeAgregujace.php
+++ funkcjeAgregujace.php
@@ -1,24 +1,39 @@
 <!DOCTYPE html>
-
 <html>
+  
 <head>
-  
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width">
 <link rel="stylesheet" href="style.css">
-<a class="mojlink0" href="https://github.com/SK-2019/php-sql-wprowadzenie-Grzesiek-Szymanek">Kliknij aby zobaczyć wszystko</a>
-  
-<div class="nav">
-    <a class="navigation_link1" href="https://wprowadzenie.herokuapp.com/">Strona Główna</a>
-    <a class="navigation_link1" href="function.php">Funkcja</a>
-    <a class="navigation_link1" href="daneDoBazy.php">Formularz</a>
-    <a class="navigation_link1" href="pracownicy.php">Pracownicy</a>
-    <a class="navigation_link1" href="funkcjeAgregujace.php">Funkcje Agregujące</a>
-    <a class="navigation_link1" href="pracownicyiorganizacja.php">Pracownicy I Organizacja</a>
-    <a class="navigation_link1" href="biblioteka.php">Biblioteka</a>
-</div>
-  
-  </head>
-  <hr></hr>
+</head>
+
 <body>
+   <div class="container">
+     
+     <div class="item colorRed">
+      <h1 class="tltle"><a class="mojlink0" href="https://github.com/SK-2019/php-sql-wprowadzenie-Grzesiek-Szymanek">WYŚWIETL KOD - GitHub</a></h1>
+     </div>
+     
+     <div class="item colorBlue">
+      <h1 style="margin-left: 15%;">MENU STRONY</h1>
+       
+         <div class="nav">
+           <a class="navigation_link1" href="https://wprowadzenie.herokuapp.com/">Strona Główna</a>
+           <a class="navigation_link1" href="function.php">Funkcja</a>
+           <a class="navigation_link1" href="daneDoBazy.php">Formularz</a>
+           <a class="navigation_link1" href="pracownicy.php">Pracownicy</a>
+           <a class="navigation_link1" href="funkcjeAgregujace.php">Funkcje Agregujące</a>
+           <a class="navigation_link1" href="pracownicyiorganizacja.php">Pracownicy I Organizacja</a>
+           <a class="navigation_link1" href="biblioteka.php">Biblioteka</a>
+           <h1 style="font-size: 19px;margin-top: 210%;text-align: center;">Autor: Grzesiek Szymanek</h1>
+         </div>
+       
+       </div>
+     
+      <div class="item colorGreen">
+        <h1 style="margin-left: 41%; color: #3cb300;">PRACOWNICY</h1>
+       
+
   
 <h1>FUNKCJE AGREGUJĄCE</h1>
   <h4 style="margin-top: 40px;">Linki do stron z odpowiednimi funkcjami</h4>
@@ -85,6 +100,6 @@ echo("<h3>3. SELECT Dzial, Max(zarobki) AS Maksimum, Min(zarobki) AS Minimum, Su
             }
         echo("</table>");
 ?>
-  
+     </div>
   </body>
 </html>
