ee0ded1dbed532b5313c7abbb867f0fb6235f130
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-12-13 18:58:19 +0100

    Update pracownicyiorganizacja.php

--- pracownicyiorganizacja.php
+++ pracownicyiorganizacja.php
@@ -1,32 +1,35 @@
 <!DOCTYPE html>
-
 <html>
+  
 <head>
-  
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width">
 <link rel="stylesheet" href="style.css">
-<a class="mojlink0" href="https://github.com/SK-2019/php-sql-wprowadzenie-Grzesiek-Szymanek">Kliknij aby zobaczyć wszystko</a>
-  
-<div class="nav">
-    <a class="navigation_link1" href="https://wprowadzenie.herokuapp.com/">Strona Główna</a>
-    <a class="navigation_link1" href="function.php">Funkcja</a>
-    <a class="navigation_link1" href="daneDoBazy.php">Formularz</a>
-    <a class="navigation_link1" href="pracownicy.php">Pracownicy</a>
-    <a class="navigation_link1" href="funkcjeAgregujace.php">Funkcje Agregujące</a>
-    <a class="navigation_link1" href="pracownicyiorganizacja.php">Pracownicy I Organizacja</a>
-    <a class="navigation_link1" href="biblioteka.php">Biblioteka</a>
-</div>
-  
-  <hr></hr>
- </head>
-<body>
-  
-<h1>PRACOWNICY I ORGANIZACJA</h1>
-  
-<h4 style="margin-top: 40px;">Linki do stron z odpowiednimi funkcjami</h4>
-  <a class="navigation_link1" href="pracownicyiorganizacja_sortowanie.php">Sortowanie Pracowników</a>
-  <a class="navigation_link1" href="pracownicyiorganizacja_ograniczanie.php">Ograniczanie Ilości Pracowników</a>
+</head>
 
-  
+<body>
+   <div class="container">
+     
+     <div class="item colorRed">
+      <h1 class="tltle"><a class="mojlink0" href="https://github.com/SK-2019/php-sql-wprowadzenie-Grzesiek-Szymanek">WYŚWIETL KOD - GitHub</a></h1>
+     </div>
+     
+     <div class="item colorBlue">
+      <h1 style="margin-left: 15%;">MENU STRONY</h1>
+       
+         <div class="nav">
+           <a class="navigation_link1" href="https://wprowadzenie.herokuapp.com/">Strona Główna</a>
+           <a class="navigation_link1" href="pracownicyiorganizacja.php">Pracownicy I Organizacja</a>
+           <a class="navigation_link1" href="pracownicyiorganizacja_sortowanie.php">Sortowanie Pracowników</a>
+           <a class="navigation_link1" href="pracownicyiorganizacja_ograniczanie.php">Ograniczanie Ilości Pracowników</a>
+           <h1 style="font-size: 19px;margin-top: 765%;text-align: center;">Autor: Grzesiek Szymanek</h1>
+         </div>
+       
+       </div>
+     
+      <div class="item colorGreen">
+        <h1 style="margin-left: 30%; color: #3cb300;">PRACOWNICY I ORGANIZACJA</h1>
+
 <?php
   
   echo("<h3 style='margin-top: 70px;'>0. SELECT * FROM pracownicy, organizacja WHERE (dzial=id_org)</h3>");
@@ -93,6 +96,6 @@
             }
         echo("</table>");
 ?>
-  
+        </div>
   </body>
 </html>
