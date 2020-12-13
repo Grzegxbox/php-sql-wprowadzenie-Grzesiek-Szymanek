2b433897d61a0fd9788c6fbb3e841d1b2f77cf77
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-12-13 18:11:51 +0100

    Update pracownicy.php

--- pracownicy.php
+++ pracownicy.php
@@ -19,12 +19,6 @@
        
          <div class="nav">
            <a class="navigation_link1" href="https://wprowadzenie.herokuapp.com/">Strona Główna</a>
-           <a class="navigation_link1" href="function.php">Funkcja</a>
-           <a class="navigation_link1" href="daneDoBazy.php">Formularz</a>
-           <a class="navigation_link1" href="pracownicy.php">Pracownicy</a>
-           <a class="navigation_link1" href="funkcjeAgregujace.php">Funkcje Agregujące</a>
-           <a class="navigation_link1" href="pracownicyiorganizacja.php">Pracownicy I Organizacja</a>
-           <a class="navigation_link1" href="biblioteka.php">Biblioteka</a>
            <h1 style="font-size: 19px;margin-top: 210%;text-align: center;">Autor: Grzesiek Szymanek</h1>
          </div>
        
