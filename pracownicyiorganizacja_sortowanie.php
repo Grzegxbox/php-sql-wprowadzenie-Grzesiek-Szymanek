fe716c6390046b337ee31973406f1fd1f84df8d3
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-12-13 19:07:22 +0100

    Update pracownicyiorganizacja_sortowanie.php

--- pracownicyiorganizacja_sortowanie.php
+++ pracownicyiorganizacja_sortowanie.php
@@ -22,20 +22,20 @@
            <a class="navigation_link1" href="pracownicyiorganizacja.php">Pracownicy I Organizacja</a>
            <a class="navigation_link1" href="pracownicyiorganizacja_sortowanie.php">Sortowanie Pracowników</a>
            <a class="navigation_link1" href="pracownicyiorganizacja_ograniczanie.php">Ograniczanie Ilości Pracowników</a>
-           <h1 style="font-size: 19px;margin-top: 305%;text-align: center;">Autor: Grzesiek Szymanek</h1>
+           <h1 style="font-size: 19px;margin-top: 400%;text-align: center;">Autor: Grzesiek Szymanek</h1>
          </div>
        
        </div>
      
       <div class="item colorGreen">
-        <h1 style="margin-left: 26.5%; color: #3cb300;">WYKORZYSTANIE FUNKCJI 'ORDER BY'</h1>
+        <h1 style="margin-left: 20%; color: #3cb300;">WYKORZYSTANIE FUNKCJI 'ORDER BY'</h1>
 
 <?php
   
-  echo("<h3 style='margin-top: 70px;'>1. SELECT * FROM pracownicy, organizacja WHERE dzial=id_org ORDER BY imie desc</h3>");
+  echo("<h3 style='margin-top: 70px; margin-left: 5%;'>1. SELECT * FROM pracownicy, organizacja WHERE dzial=id_org ORDER BY imie desc</h3>");
     $conn = new mysqli("remotemysql.com","Rp4CxP6YkY","V0BMRFi2V3","Rp4CxP6YkY");
     $result = $conn -> query('SELECT * FROM pracownicy, organizacja WHERE dzial=id_org ORDER BY imie desc');
-        echo("<table border=1>");
+        echo("<table style='margin-left: 5%;' border=2>");
         echo("<th>ID</th>");
         echo("<th>Imie</th>");
         echo("<th>Dzial</th>");
@@ -48,10 +48,10 @@
             }
         echo("</table>");
   
-  echo("<h3>2. SELECT * FROM pracownicy, organizacja WHERE (dzial=id_org) AND (dzial=3) ORDER BY imie asc</h3>");
+  echo("<h3 style='margin-top: 70px; margin-left: 5%;'>2. SELECT * FROM pracownicy, organizacja WHERE (dzial=id_org) AND (dzial=3) ORDER BY imie asc</h3>");
     $conn = new mysqli("remotemysql.com","Rp4CxP6YkY","V0BMRFi2V3","Rp4CxP6YkY");
     $result = $conn -> query('SELECT * FROM pracownicy, organizacja WHERE (dzial=id_org) AND (dzial=3) ORDER BY imie asc');
-        echo("<table border=1>");
+        echo("<table style='margin-left: 5%;' border=2>");
         echo("<th>ID</th>");
         echo("<th>Imie</th>");
         echo("<th>Dzial</th>");
@@ -64,10 +64,10 @@
             }
         echo("</table>");
   
-  echo("<h3>3. SELECT * FROM pracownicy, organizacja WHERE (dzial=id_org) AND (imie like '%a') ORDER BY imie asc</h3>");
+  echo("<h3 style='margin-top: 70px; margin-left: 5%;'>3. SELECT * FROM pracownicy, organizacja WHERE (dzial=id_org) AND (imie like '%a') ORDER BY imie asc</h3>");
     $conn = new mysqli("remotemysql.com","Rp4CxP6YkY","V0BMRFi2V3","Rp4CxP6YkY");
     $result = $conn -> query('SELECT * FROM pracownicy, organizacja WHERE (dzial=id_org) AND (imie like "%a") ORDER BY imie asc');
-        echo("<table border=1>");
+        echo("<table style='margin-left: 5%;' border=2>");
         echo("<th>ID</th>");
         echo("<th>Imie</th>");
         echo("<th>Dzial</th>");
@@ -80,10 +80,10 @@
             }
         echo("</table>");
   
-  echo("<h3>4. SELECT * FROM pracownicy, organizacja WHERE (dzial=id_org) AND (imie like '%a') ORDER BY zarobki asc</h3>");
+  echo("<h3 style='margin-top: 70px; margin-left: 5%;'>4. SELECT * FROM pracownicy, organizacja WHERE (dzial=id_org) AND (imie like '%a') ORDER BY zarobki asc</h3>");
     $conn = new mysqli("remotemysql.com","Rp4CxP6YkY","V0BMRFi2V3","Rp4CxP6YkY");
     $result = $conn -> query('SELECT * FROM pracownicy, organizacja WHERE (dzial=id_org) AND (imie like "%a") ORDER BY zarobki asc');
-        echo("<table border=1>");
+        echo("<table style='margin-left: 5%;' border=2>");
         echo("<th>ID</th>");
         echo("<th>Imie</th>");
         echo("<th>Dzial</th>");
@@ -96,10 +96,10 @@
             }
         echo("</table>");
   
-  echo("<h3>5. SELECT * FROM pracownicy, organizacja WHERE (dzial=id_org) AND (imie not like '%a') ORDER BY nazwa_dzial asc, zarobki asc</h3>");
+  echo("<h3 style='margin-top: 70px; margin-left: 5%;'>5. SELECT * FROM pracownicy, organizacja WHERE (dzial=id_org) AND (imie not like '%a') ORDER BY nazwa_dzial asc, zarobki asc</h3>");
     $conn = new mysqli("remotemysql.com","Rp4CxP6YkY","V0BMRFi2V3","Rp4CxP6YkY");
     $result = $conn -> query('SELECT * FROM pracownicy, organizacja WHERE (dzial=id_org) AND (imie not like "%a") ORDER BY nazwa_dzial asc, zarobki asc');
-        echo("<table border=1>");
+        echo("<table style='margin-left: 5%;' border=2>");
         echo("<th>ID</th>");
         echo("<th>Imie</th>");
         echo("<th>Dzial</th>");
