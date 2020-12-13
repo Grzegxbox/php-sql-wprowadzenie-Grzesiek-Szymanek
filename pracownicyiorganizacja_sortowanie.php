837ab003870af651794ed1a0fe75d62cb881e651
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-12-13 19:03:22 +0100

    Update pracownicyiorganizacja_sortowanie.php

--- pracownicyiorganizacja_sortowanie.php
+++ pracownicyiorganizacja_sortowanie.php
@@ -2,20 +2,34 @@
 <html>
   
 <head>
-  
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width">
 <link rel="stylesheet" href="style.css">
-<a class="mojlink0" href="https://github.com/SK-2019/php-sql-wprowadzenie-Grzesiek-Szymanek">Kliknij aby zobaczyć wszystko</a>
-  
-<div class="nav">
-  <a class="navigation_link1" href="pracownicyiorganizacja.php">Pracownicy I Organizacja</a>
-</div>
-  
-  <hr></hr>
- </head>
+</head>
+
 <body>
-  
-<h1>WYKORZYSTANIE FUNKCJI 'ORDER BY'</h1>
- 
+   <div class="container">
+     
+     <div class="item colorRed">
+      <h1 class="tltle"><a class="mojlink0" href="https://github.com/SK-2019/php-sql-wprowadzenie-Grzesiek-Szymanek">WYŚWIETL KOD - GitHub</a></h1>
+     </div>
+     
+     <div class="item colorBlue">
+      <h1 style="margin-left: 15%;">MENU STRONY</h1>
+       
+         <div class="nav">
+           <a class="navigation_link1" href="https://wprowadzenie.herokuapp.com/">Strona Główna</a>
+           <a class="navigation_link1" href="pracownicyiorganizacja.php">Pracownicy I Organizacja</a>
+           <a class="navigation_link1" href="pracownicyiorganizacja_sortowanie.php">Sortowanie Pracowników</a>
+           <a class="navigation_link1" href="pracownicyiorganizacja_ograniczanie.php">Ograniczanie Ilości Pracowników</a>
+           <h1 style="font-size: 19px;margin-top: 305%;text-align: center;">Autor: Grzesiek Szymanek</h1>
+         </div>
+       
+       </div>
+     
+      <div class="item colorGreen">
+        <h1 style="margin-left: 26.5%; color: #3cb300;">WYKORZYSTANIE FUNKCJI 'ORDER BY'</h1>
+
 <?php
   
   echo("<h3 style='margin-top: 70px;'>1. SELECT * FROM pracownicy, organizacja WHERE dzial=id_org ORDER BY imie desc</h3>");
@@ -98,6 +112,6 @@
             }
         echo("</table>");
 ?>
-  
+    </div>
   </body>
 </html>
