f29939166803a12042baafd8c8db59da82b55eb7
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-12-13 17:49:02 +0100

    Update pracownicy.php

--- pracownicy.php
+++ pracownicy.php
@@ -1,28 +1,38 @@
 <!DOCTYPE html>
-
 <html>
+  
 <head>
-  
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width">
 <link rel="stylesheet" href="style.css">
-<a class="mojlink0" href="https://github.com/SK-2019/php-sql-wprowadzenie-Grzesiek-Szymanek">Kliknij aby zobaczyć wszystko</a>
-  
-<div class="nav">
-    <a class="navigation_link1" href="https://wprowadzenie.herokuapp.com/">Strona Główna</a>
-    <a class="navigation_link1" href="function.php">Funkcja</a>
-    <a class="navigation_link1" href="daneDoBazy.php">Formularz</a>
-    <a class="navigation_link1" href="pracownicy.php">Pracownicy</a>
-    <a class="navigation_link1" href="funkcjeAgregujace.php">Funkcje Agregujące</a>
-    <a class="navigation_link1" href="pracownicyiorganizacja.php">Pracownicy I Organizacja</a>
-    <a class="navigation_link1" href="biblioteka.php">Biblioteka</a>
-</div>
-  
-  <hr></hr>
- </head>
+</head>
+
 <body>
-    
-  
-<h1>PRACOWNICY</h1>
- 
+   <div class="container">
+     
+     <div class="item colorRed">
+      <h1 class="tltle"><a class="mojlink0" href="https://github.com/SK-2019/php-sql-wprowadzenie-Grzesiek-Szymanek">WYŚWIETL KOD - GitHub</a></h1>
+     </div>
+     
+     <div class="item colorBlue">
+      <h1 style="margin-left: 15%;">MENU STRONY</h1>
+       
+         <div class="nav">
+           <a class="navigation_link1" href="https://wprowadzenie.herokuapp.com/">Strona Główna</a>
+           <a class="navigation_link1" href="function.php">Funkcja</a>
+           <a class="navigation_link1" href="daneDoBazy.php">Formularz</a>
+           <a class="navigation_link1" href="pracownicy.php">Pracownicy</a>
+           <a class="navigation_link1" href="funkcjeAgregujace.php">Funkcje Agregujące</a>
+           <a class="navigation_link1" href="pracownicyiorganizacja.php">Pracownicy I Organizacja</a>
+           <a class="navigation_link1" href="biblioteka.php">Biblioteka</a>
+           <h1 style="font-size: 19px;margin-top: 380%;text-align: center;">Autor: Grzesiek Szymanek</h1>
+         </div>
+       
+       </div>
+     
+      <div class="item colorGreen">
+        <h1 style="margin-left: 38%; color: #3cb300;">PRACOWNICY</h1>
+       
 <?php
   
   echo("<h3 style='margin-top: 60px;'>0. SELECT * FROM pracownicy, organizacja WHERE (dzial=id_org)</h3>");
@@ -86,6 +96,6 @@
             }
         echo("</table>");
 ?>
-  
+     </div>
   </body>
 </html>
