d58ea240425d0b406cca15af6e4fd3e631e51dc8
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-12-14 18:23:13 +0100

    Update wyporzyczone.php

--- wyporzyczone.php
+++ wyporzyczone.php
@@ -1,22 +1,41 @@
 <!DOCTYPE html>
 <html>
- 
+  
 <head>
- 
- <link rel="stylesheet" href="style.css">
- <a class="mojlink0" href="https://github.com/SK-2019/php-sql-wprowadzenie-Grzesiek-Szymanek">Kliknij aby zobaczyć wszystko</a>
- 
- <div class="nav">
-    <a class="navigation_link1" href="https://wprowadzenie.herokuapp.com/">Strona Główna</a>
-  </div>
- 
- <hr></hr>
- 
- <meta charset="utf-8">
- <meta name="viewport" content="width=device-width">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width">
+<link rel="stylesheet" href="style.css">
+</head>
 
-<?php
+<body>
+   <div class="container">
+     <div class="item colorRed">
+       
+       <h1 class="tltle"><a class="mojlink0" href="https://github.com/SK-2019/php-sql-wprowadzenie-Grzesiek-Szymanek">WYŚWIETL KOD - GitHub</a></h1>
+       
+     </div>
+     
+     <div class="item colorBlue">
+       
+        <h1 style="margin-left: 15%;">MENU STRONY</h1>
+       
+         <div class="nav">
+           <a class="navigation_link1" href="https://wprowadzenie.herokuapp.com/">Strona Główna</a>
+           <a class="navigation_link1" href="biblioteka.php">Biblioteka</a>
+           <a class="navigation_link1" href="wyporzyczone.php">Wypożycz książkę</a>
+           <a class="navigation_link1" href="oddane.php">Oddaj książkę</a>
+           <h1 style="font-size: 19px;margin-top: 250%;text-align: center;">Autor: Grzesiek Szymanek</h1>
+         </div>
+       </div>
+      <div class="item colorGreen">
+        
+       <h1 style="margin-left: 41.5%; color: #3cb300;">WYPOŻYCZ KSIĄŻKĘ</h1>
+       
+  <?php
 
 echo("<h1>Tutaj będą książki wypożyczone:)</h1>");
 
 ?>
+    </div>
+    </body>
+ </html>
