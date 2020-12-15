7d46bf329582180783d40b22e116fb8760572cae
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-12-15 14:22:19 +0100

    Create insert_autor.php

---
+++ insert_autor.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+  
+  <head>
+   <div>
+    <a class="navigation_link1" href="https://wprowadzenie.herokuapp.com/">Strona Główna</a>
+  </div>
+      <link rel="stylesheet" href="style.css">
+    <hr></hr>
+  </head>
+  
+<body>
+  <h1>TWOJE DANE</h1>
+
+<?php
+
+echo("<h3>ID:</h3>");
+echo("<h3>".$_POST['id']."</h3>");
+  
+echo("<h3>AUTOR:</h3>");  
+echo("<h3>".$_POST['autor']."</h3>");
+  
+$servername = "remotemysql.com";
+$username = "Rp4CxP6YkY";
+$password = "V0BMRFi2V3";
+$dbname = "Rp4CxP6YkY";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO biblAutor (id, autor)
+VALUES(NULL,'".$_POST['id']."', '".$_POST['autor']."', '""', '""')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "WŁAŚNIE SIĘ DODAŁEŚ";
+  header('Location: https://wprowadzenie.herokuapp.com/');
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+  
+// Tabelka  
+$conn= new mysqli("remotemysql.com","Rp4CxP6YkY","V0BMRFi2V3","Rp4CxP6YkY");
+    $result=$conn->query('SELECT * FROM pracownicy, organizacja WHERE (dzial=id_org)');
+        echo("<table border=2>");
+        echo("<th>ID</th>");
+        echo("<th>IMIĘ</th>");
+        echo("<th>DZIAŁ</th>");
+        echo("<th>NAZWA DZIAŁU</th>");
+        echo("<th>ZAROBKI</th>");
+        echo("<th>DATA URODZENIA</th>");
+            while($row=$result->fetch_assoc()){
+                echo("<tr>");
+                    echo("<td>" .$row["id_pracownicy"]. "</td><td>" .$row["imie"]. "</td><td>" .$row["dzial"]. "</td><td>" .$row["nazwa_dzial"]. "</td><td>" .$row["zarobki"]. "</td><td>" .$row["data_urodzenia"]. "</td>");
+                echo("</tr>");
+            }
+        echo("</table>");
+
+?>
