2d1a6907b001a2baaa8c537a4ba4205242887410
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-12-15 14:14:20 +0100

    Update daneDoAutorow.php

--- daneDoAutorow.php
+++ daneDoAutorow.php
@@ -20,22 +20,22 @@
            <a class="navigation_link1" href="daneDoBazy.php">Formularz - Pracownicy</a>
            <a class="navigation_link1" href="daneDoAutorow.php">Formularz - Autorzy</a>
            <a class="navigation_link1" href="daneDoTytulow.php">Formularz - Książki</a>
-           <h1 style="font-size: 19px;margin-top: 40%;text-align: center;">Autor: Grzesiek Szymanek</h1>
+           <h1 style="font-size: 19px;margin-top: 150%;text-align: center;">Autor: Grzesiek Szymanek</h1>
        </div>
       </div>
      <div class="item colorGreen">
-       <h1 style="margin-left: 29%; color: #3cb300;">FORMULARZ - AUTORZY</h1>
+       <h1 style="margin-left: 32.5%; color: #3cb300;">FORMULARZ - AUTORZY</h1>
           <div class="dodawanie" style="margin-top: 7%;">
-            <h2 style="margin-left: 8%;color: #2E8B57;">DODAWANIE AUTORA</h2>
-               <form action=".php" method="POST" style="margin-left: 19%;">
+            <h2 style="margin-left: 11%;color: #2E8B57;">DODAWANIE AUTORA</h2>
+               <form action=".php" method="POST" style="margin-left: 17%;">
                  <input type="text" name="id" placeholder="ID"><br>
                  <input type="text" name="autor" placeholder="AUTOR"><br>
                  <input type="submit" value="DODAJ" style="margin-left: 40px;">
                </form>
             </div>
               <div class="odejmowanie">
-                 <h2 style="color: #2E8B57;">USUWANIE AUTORA</h2>
-                   <form action=".php" method="POST" style="margin-left: 27%;">
+                 <h2 style="color: #2E8B57; margin-top: 14%; margin-left: 12%;">USUWANIE AUTORA</h2>
+                   <form action=".php" method="POST" style="margin-left: 28%;">
                      <input type="text" name="id" placeholder="ID AUTORA"><br>
                      <input type="submit" value="USUŃ" style="margin-left: 13%;">
                    </form>
@@ -44,7 +44,7 @@
   
   $conn= new mysqli("remotemysql.com","Rp4CxP6YkY","V0BMRFi2V3","Rp4CxP6YkY");
     $result=$conn->query('SELECT * FROM biblAutor');
-        echo("<table style='margin-top: 15%;margin-left: 21%;' border=2>");
+        echo("<table style='margin-top: 22%;margin-left: -47%;' border=2>");
         echo("<th>ID</th>");
         echo("<th>AUTOR</th>");
         echo("<th>USUŃ</th>");
