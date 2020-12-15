28fd968db4a518e6d1aa8a9a35ac4a34bb413262
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-12-15 15:29:46 +0100

    Create delete_tytul.php

---
+++ delete_tytul.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+     <div>
+    <a class="navigation_link1" href="https://wprowadzenie.herokuapp.com/">Strona Główna</a>
+  </div>
+    <hr></hr>
+  <link rel="stylesheet" href="style.css">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>Dane do Bazy</title>
+</head>
+<body>
+
+<?php
+  
+echo("<h1>ID: ".$_POST['id']."</h1>");
+  
+$servername = "remotemysql.com";
+$username = "Rp4CxP6YkY";
+$password = "V0BMRFi2V3";
+$dbname = "Rp4CxP6YkY";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+
+$sql = "DELETE FROM biblTytul WHERE id=('".$_POST['id']."')";
+
+if ($conn->query($sql) === TRUE){
+  echo("KSIĄŻKA ZOSTAŁA USUNIĘTA");
+  header('Location: https://wprowadzenie.herokuapp.com/biblioteka.php');
+  } else  {
+  echo('ERROR: ' .$sql. '<br>' .$conn->error);
+  }
+  $conn->close();
+
+?>
+
+  </body>
+</html>
