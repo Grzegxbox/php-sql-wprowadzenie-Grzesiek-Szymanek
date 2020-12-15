c6546453a8eeeb23a742f012131930c19bc01e63
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-12-15 16:25:12 +0100

    Update oddane.php

--- oddane.php
+++ oddane.php
@@ -29,7 +29,7 @@
        </div>
       <div class="item colorGreen">
         
-       <h1 style="margin-left: 38%; color: #3cb300;">ODDAJ KSIĄŻKĘ</h1>
+       <h1 style="margin-left: 38.5%; color: #3cb300;">ODDAJ KSIĄŻKĘ</h1>
        
   <?php
 
