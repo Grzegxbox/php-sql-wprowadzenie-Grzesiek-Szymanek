622c2d9a8406d95efa5ff201ab627e300963c3e8
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-12-16 23:26:23 +0100

    Update function.php

--- function.php
+++ function.php
@@ -9,11 +9,9 @@
 
 <body>
    <div class="container">
-    
     <div class="item colorRed"> 
      <h1 class="tltle"><a class="mojlink0" href="https://github.com/SK-2019/php-sql-wprowadzenie-Grzesiek-Szymanek">WYŚWIETL KOD - GitHub</a></h1>
     </div>
-    
     <div class="item colorBlue"> 
      <h1 style="margin-left: 15%;">MENU STRONY</h1>
       <div class="nav">
@@ -21,13 +19,11 @@
            <h1 style="font-size: 19px;margin-top: 32330%;text-align: center;">Autor: Grzesiek Szymanek</h1>
       </div>
      </div>
-    
      <div class="item colorGreen">
       <h1 style="margin-left: 45%; color: #3cb300;">FUNKCJA</h1>  
 
 <?php
-    
-
+  
 function liczby($liczba){
     echo("<h3 style='margin-top: 6%;margin-left: 5%;color: green;'>Początek funkcji </h3>");
     echo("<h4 style='margin-left: 5%;'>Napisz Liczby Do ".$liczba);
