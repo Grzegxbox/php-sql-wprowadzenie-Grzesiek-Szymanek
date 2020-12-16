b51f71c6934227813dc809d4e1fecd4a218aa5d0
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-12-16 23:22:06 +0100

    Update connect.php

--- connect.php
+++ connect.php
@@ -10,6 +10,7 @@ $dbname = " Rp4CxP6YkY";
 // Create connection (łącze się z moją bazą danych)
 $conn = new mysqli($servername, $username, $password, $dbname);
 
+
 // Check connection (sprawdzam połączenie z bazą danych)
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
