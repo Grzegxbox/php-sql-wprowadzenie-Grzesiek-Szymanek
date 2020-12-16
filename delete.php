0bbd0c8199d6008431f6ba0e1dd0e628163ae81e
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2020-12-16 23:30:02 +0100

    Update delete.php

--- delete.php
+++ delete.php
@@ -27,7 +27,6 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-
 $sql = "DELETE FROM pracownicy WHERE id_pracownicy=('".$_POST['id']."')";
 
 if ($conn->query($sql) === TRUE){
