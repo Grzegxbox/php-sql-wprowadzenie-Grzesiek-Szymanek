6542ef3ccdb8e1a47a0664ef7bb5d46ef41fbf31
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2021-01-26 13:39:46 +0100

    zmiany odnośnie katalogów

--- PRACOWNICY/pracownicyiorganizacja.php
+++ PRACOWNICY/pracownicyiorganizacja.php
@@ -4,7 +4,7 @@
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width">
-<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="ASSETS/style.css">
 </head>
 
 <body>
@@ -18,9 +18,9 @@
        
          <div class="nav">
            <a class="navigation_link1" href="https://wprowadzenie.herokuapp.com/">Strona Główna</a>
-           <a class="navigation_link1" href="pracownicyiorganizacja.php">Pracownicy I Organizacja</a>
-           <a class="navigation_link1" href="pracownicyiorganizacja_sortowanie.php">Sortowanie Pracowników</a>
-           <a class="navigation_link1" href="pracownicyiorganizacja_ograniczanie.php">Ograniczanie Ilości Pracowników</a>
+           <a class="navigation_link1" href="PRACOWNICY/pracownicyiorganizacja.php">Pracownicy I Organizacja</a>
+           <a class="navigation_link1" href="PRACOWNICY/pracownicyiorganizacja_sortowanie.php">Sortowanie Pracowników</a>
+           <a class="navigation_link1" href="PRACOWNICY/pracownicyiorganizacja_ograniczanie.php">Ograniczanie Ilości Pracowników</a>
            <h1 style="font-size: 19px;margin-top: 305%;text-align: center;">Autor: Grzesiek Szymanek</h1>
          </div>
        
