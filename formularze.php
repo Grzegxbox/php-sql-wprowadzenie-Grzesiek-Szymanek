e015c4be5985308e7f4475de4e549546c67a48fa
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2021-01-26 13:19:55 +0100

    Update formularze.php

--- formularze.php
+++ formularze.php
@@ -10,23 +10,24 @@
 <body>
   <div class="container">
     <div class="item colorRed">
-     <h1 class="tltle"><a class="mojlink0" href="https://github.com/SK-2019/php-sql-wprowadzenie-Grzesiek-Szymanek">WYŚWIETL KOD - GitHub</a></h1>
+    <h1 class="tltle"><a class="mojlink0" href="https://github.com/SK-2019/php-sql-wprowadzenie-Grzesiek-Szymanek">WYŚWIETL KOD - GitHub</a></h1>
     </div>
       <div class="item colorBlue">
-       <h1 style="margin-left: 15%;">MENU STRONY</h1>
-        <div class="nav">
-          <a class="navigation_link1" href="https://wprowadzenie.herokuapp.com/">Strona Główna</a>
-          <a class="navigation_link1" href="formularze.php">Formularze</a>
-          <a class="navigation_link1" href="daneDoBazy.php">Formularz - Pracownicy</a>
-          <a class="navigation_link1" href="daneDoAutorow.php">Formularz - Autorzy</a>
-          <a class="navigation_link1" href="daneDoTytulow.php">Formularz - Książki</a>
-          <h1 style="font-size: 19px;margin-top: 40%;text-align: center;">Autor: Grzesiek Szymanek</h1>
-        </div>
+      <h1 style="margin-left: 15%;">MENU STRONY</h1>
+       <div class="nav">
+       <a class="navigation_link1" href="https://wprowadzenie.herokuapp.com/">Strona Główna</a>
+       <a class="navigation_link1" href="formularze.php">Formularze</a>
+       <a class="navigation_link1" href="daneDoBazy.php">Formularz - Pracownicy</a>
+       <a class="navigation_link1" href="daneDoAutorow.php">Formularz - Autorzy</a>
+       <a class="navigation_link1" href="daneDoTytulow.php">Formularz - Książki</a>
+       <h1 style="font-size: 19px;margin-top: 40%;text-align: center;">Autor: Grzesiek Szymanek</h1>
+       </div>
       </div>
-     <div class="item colorGreen">
-       <h1 style="margin-left: 41%;">FORMULARZE</h1>
-       <h2 style="margin-top: 11%; margin-left: 10%; margin-right: 5%; color: #3cb371;">W tym miejscu możesz przejeść do odpowiednich zakładek aby dodać/usunąć pracownika, autora książki lub samą książkę.</h2>
-     </div>
+        <div class="item colorGreen">
+        <h1 style="margin-left: 41%;">FORMULARZE</h1>
+        <h2 style="margin-top: 11%; margin-left: 10%; margin-right: 5%; color: #3cb371;">W tym miejscu możesz przejeść do odpowiednich zakładek aby dodać/usunąć pracownika, autora książki lub samą książkę.</h2>
+        </div>
   </div>
- </body>
+  
+</body>
 </html>
