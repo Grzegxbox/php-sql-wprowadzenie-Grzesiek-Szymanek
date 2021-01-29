c6461d1df7af8dce65f687ae87db73e7541e3081
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2021-01-29 13:07:50 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>     
   
 <head>
-<?php require_once("/ASSETS/nazwastrony.php"); ?>
+<?php require("/ASSETS/nazwastrony.php"); ?>
 <meta charset="utf-8">
 <link rel="stylesheet" href="/ASSETS/style.css">
 <meta name="viewport" content="width=device-width">
