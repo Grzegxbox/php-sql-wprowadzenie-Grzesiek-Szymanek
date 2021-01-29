c8521c986a7fb42be012b52a9e68ede6ac9a8579
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2021-01-29 14:01:51 +0100

    Update oddane.php

--- BIBLIOTEKA/oddane.php
+++ BIBLIOTEKA/oddane.php
@@ -5,6 +5,7 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width">
 <link rel="stylesheet" href="/ASSETS/style.css">
+<link rel="icon" href="https://i.ytimg.com/vi/iu-skHVnxGc/hqdefault.jpg">
 </head>
 
 <body>
