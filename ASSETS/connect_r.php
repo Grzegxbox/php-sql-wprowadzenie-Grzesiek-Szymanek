414ef40de3c08a35b3c48e63dcd335b41498afb6
Grzesiek-Szymanek <szymanek.grzegorz.2ti@gmail.com>
2021-02-23 18:49:07 +0100

    r

--- ASSETS/connect_r.php
+++ ASSETS/connect_r.php
@@ -1,10 +1,10 @@
 <?php
-$servername = $_SERVER_r['servername'];
-$username = $_SERVER_r['username'];
-$password = $_SERVER_r['password'];
-$dbname = $_SERVER_r['dbname']; 
+$servername = $_SERVER_r['servername_r'];
+$username = $_SERVER_r['username_r'];
+$password = $_SERVER_r['password_r'];
+$dbname = $_SERVER_r['dbname_r']; 
 
-$conn= new mysqli($_SERVER_r['servername'],$_SERVER_r['username'],$_SERVER_r['password'],$_SERVER_r['dbname']);
+$conn= new mysqli($_SERVER_r['servername_r'],$_SERVER_r['username_r'],$_SERVER_r['password_r'],$_SERVER_r['dbname_r']);
 if ($conn->connect_error) {
     die("connection failed: ".mysqli_connect_error());
 }
