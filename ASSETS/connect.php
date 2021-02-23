a8e64879334c9292a7d68d3d8096bfdb2aa74076
Grzesiek-Szymanek <szymanek.grzegorz.2ti@gmail.com>
2021-02-23 14:01:49 +0100

    s

--- ASSETS/connect.php
+++ ASSETS/connect.php
@@ -1,8 +1,6 @@
 <?php
 // Add your data (jak by co to nie mam takich nazw użytkownika, haseł i nazwy serwera) - remotemysql.com","QcPKWLShOa","KnxvVt5XqH","QcPKWLShOa (na jedna osobe)
 
-
-
 $conn= new mysqli($_SERVER['servername'],$_SERVER['username'],$_SERVER['password'],$_SERVER['dbname']);
 if ($conn->connect_error) {
     die("connection failed: ".mysqli_connect_error());
