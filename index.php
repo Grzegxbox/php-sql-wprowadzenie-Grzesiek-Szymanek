847a8afce3ef48d2014357cd6b3c5b1e57ffdc38
Grzesiek-Szymanek <szymanek.grzegorz.2ti@gmail.com>
2021-02-23 17:39:11 +0100

    j

--- index.php
+++ index.php
@@ -38,7 +38,7 @@
        <?php
         
 
-          require_once("config.php");
+          require_once("connect.php");
 
         
 //  echo("<h3 style='margin-top: 50px; margin-left: 5%; color: #3cb371;'>0. SELECT * FROM pracownicy</h3>");
