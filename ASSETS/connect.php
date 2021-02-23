6189823fe418600ae8a354dbd7bc2d3b9231905b
Grzesiek-Szymanek <szymanek.grzegorz.2ti@gmail.com>
2021-02-23 17:08:29 +0100

    zle dane

--- ASSETS/connect.php
+++ ASSETS/connect.php
@@ -1,5 +1,4 @@
 <?php
-// Add your data (jak by co to nie mam takich nazw użytkownika, haseł i nazwy serwera) - remotemysql.com","QcPKWLShOa","KnxvVt5XqH","QcPKWLShOa (na jedna osobe)
 
 $conn= new mysqli($_SERVER['servername'],$_SERVER['username'],$_SERVER['password'],$_SERVER['dbname']);
 if ($conn->connect_error) {
