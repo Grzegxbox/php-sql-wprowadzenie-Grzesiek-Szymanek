c0c0d246c185b4655a435a8fab3df9a3c89cadb1
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2021-02-23 17:03:41 +0100

    Update index.php

--- index.php
+++ index.php
@@ -37,9 +37,9 @@
        
        <?php
         
-        require("../ASSETS/connect.php");
-
-    echo("<h3 style='margin-top: 50px; margin-left: 5%; color: #3cb371;'>0. SELECT * FROM pracownicy</h3>");
+        require_once('ASSETS/connect.php');
+        
+//  echo("<h3 style='margin-top: 50px; margin-left: 5%; color: #3cb371;'>0. SELECT * FROM pracownicy</h3>");
     $result=$conn->query('SELECT * FROM pracownicy');
         echo("<table border=2 style=' margin-left: 5%;'>");
         echo("<th>ID</th>");
