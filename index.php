25a5c9e50f06580f29cbb1e6f46678e9919e7fc5
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2021-02-23 16:47:31 +0100

    Update index.php

--- index.php
+++ index.php
@@ -39,7 +39,7 @@
         
 //         $hostname = $_SERVER['HTTP_HOST'];
 //         if ($hostname == 'localhost:8003') {
-            require_once("/ASSETS/connect.php");
+            require_once("config.php");
 //         }
 
     echo("<h3 style='margin-top: 50px; margin-left: 5%; color: #3cb371;'>0. SELECT * FROM pracownicy</h3>");
