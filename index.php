cfb1d9577e8e015eaff0d26486abb9494986b2f8
Grzesiek_Szymanek <szymanek.grzegorz2ti@gmail.com>
2021-03-17 19:47:29 +0100

    commit

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>   
 <html>  
-<!-- commit podczas nagrywania -->
+
 <head>
 <title>Grzesiek Szymanek 2TI gr2</title>
 <meta charset="utf-8">
@@ -31,7 +31,7 @@
      <div class="item colorGreen">
         <h1 style="margin-left: 37%; color: #3cb300;">STRONA GŁÓWNA</h1>
        <?php
-        
+        echo("<h1>TEST</h1>");
          require_once("ASSETS/connect.php");
         
     echo("<h3 style='margin-top: 50px; margin-left: 5%; color: #3cb371;'>0. SELECT * FROM pracownicy</h3>");
