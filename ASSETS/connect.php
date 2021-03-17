1b851a0100081be3e8bb84cb92b43ae4a01d08ff
Grzesiek_Szymanek <szymanek.grzegorz2ti@gmail.com>
2021-03-17 20:51:51 +0100

    hejka

--- ASSETS/connect.php
+++ ASSETS/connect.php
@@ -1,11 +1,13 @@
 <?php
 
-$servername = $_SERVER['db'];
-$username = $_SERVER['root'];
-$password = $_SERVER['root'];
-$dbname = $_SERVER['tom']; 
+echo("<li>info z dockera,:) ")";
 
-$conn= new mysqli($_SERVER['servername'],$_SERVER['username'],$_SERVER['password'],$_SERVER['dbname']);
+$servername = "db";
+$username = "root";
+$password = "root";
+$dbname = "tom";
+
+$conn= new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("connection failed: ".mysqli_connect_error());
 }
