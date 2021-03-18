99f9bfcb81bba46454490f1828b17eb24f71d007
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2021-03-18 11:50:04 +0100

    Update connect.php

--- ASSETS/connect.php
+++ ASSETS/connect.php
@@ -1,10 +1,10 @@
 <?php
-$servername = $_SERVER['servername_r'];
-$username = $_SERVER['username_r'];
-$password = $_SERVER['password_r'];
-$dbname = $_SERVER['dbname_r']; 
+$servername = $_SERVER['servername'];
+$username = $_SERVER['username'];
+$password = $_SERVER['password'];
+$dbname = $_SERVER['dbname']; 
 
-$conn= new mysqli($_SERVER['servername_r'],$_SERVER['username_r'],$_SERVER['password_r'],$_SERVER['dbname_r']);
+$conn= new mysqli($_SERVER['servername'],$_SERVER['username'],$_SERVER['password_'],$_SERVER['dbname']);
 if ($conn->connect_error) {
     die("connection failed: ".mysqli_connect_error());
 }
