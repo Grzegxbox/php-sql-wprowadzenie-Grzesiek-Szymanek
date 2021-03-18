ff9533100dba2730dab8c2c078fc0cb4f0e5dbc5
Grzesiek-Szymanek <szymanek.grzegorz.2ti@gmail.com>
2021-03-18 13:05:33 +0100

    trunde to jest fest

--- ASSETS/connect.php
+++ ASSETS/connect.php
@@ -1,14 +1,25 @@
 <?php
 
-require_once('config.php');
 
-$servername = $_SERVER['servername'];
-$username = $_SERVER['username'];
-$password = $_SERVER['password'];
-$dbname = $_SERVER['dbname']; 
+$hostname = $_SERVER['HTTP_HOST'];
+if ($hostname === '127.0.0.1'){
+    require_once("config.php");}
+    
+    $conn= new mysqli($_SERVER['servername'],$_SERVER['username'],$_SERVER['password'],$_SERVER['dbname']);
+    if ($conn->connect_error) {
+        die("connection failed: ".mysqli_connect_error());
+    }
 
-$conn= new mysqli($_SERVER['servername'],$_SERVER['username'],$_SERVER['password_'],$_SERVER['dbname']);
-if ($conn->connect_error) {
-    die("connection failed: ".mysqli_connect_error());
-}
+
+// require_once('config.php');
+
+// $servername = $_SERVER['servername'];
+// $username = $_SERVER['username'];
+// $password = $_SERVER['password'];
+// $dbname = $_SERVER['dbname']; 
+
+// $conn= new mysqli($_SERVER['servername'],$_SERVER['username'],$_SERVER['password_'],$_SERVER['dbname']);
+// if ($conn->connect_error) {
+//     die("connection failed: ".mysqli_connect_error());
+// }
 ?>
