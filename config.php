96476d10db231ac098d158df1818ecdfbe9b7059
Grzesiek-Szymanek <szymanek.grzegorz.2ti@gmail.com>
2021-03-18 12:07:05 +0100

    d

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $_SERVER['servername'] = "remotemysql.com";
-    $_SERVER['username'] = "Rp4CxP6YkY";
-    $_SERVER['password'] = "V0BMRFi2V3";
-    $_SERVER['dbname'] = "Rp4CxP6YkY";
-?>
