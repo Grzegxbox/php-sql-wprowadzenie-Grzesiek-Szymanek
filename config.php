267430471ef51b053af1e44ad39c7c44efbf1b06
Grzesiek Szymanek <73532579+Grzesiek-Szymanek@users.noreply.github.com>
2021-03-18 11:53:25 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
     $_SERVER['servername'] = "remotemysql.com";
-    $_SERVER['username'] = "QcPKWLShOa";
-    $_SERVER['password'] = "KnxvVt5XqH";
-    $_SERVER['dbname'] = "QcPKWLShOa";
+    $_SERVER['username'] = "Rp4CxP6YkY";
+    $_SERVER['password'] = "V0BMRFi2V3";
+    $_SERVER['dbname'] = "Rp4CxP6YkY";
 ?>
