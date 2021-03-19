cc1e5f5933e4bea23883f0637f3ed35736bcae25
Grzesiek-Szymanek <szymanek.grzegorz.2ti@gmail.com>
2021-03-19 12:37:52 +0100

    .

--- ASSETS/connect.php
+++ ASSETS/connect.php
@@ -1,12 +1,7 @@
 <?php
-require_once('config.php');
-$servername = $_SERVER['servername'];
-$username = $_SERVER['username'];
-$password = $_SERVER['password'];
-$dbname = $_SERVER['dbname']; 
 
 $conn= new mysqli($_SERVER['servername'],$_SERVER['username'],$_SERVER['password_'],$_SERVER['dbname']);
 if ($conn->connect_error) {
-    die("connection failed: ".mysqli_connect_error());
+    die("connection failed: ". $conn->connect_error);
 }
 ?>
